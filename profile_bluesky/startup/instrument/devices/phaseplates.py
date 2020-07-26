d707e6290ac998f7c629429aac8522b6173776a0
gfabbris <gfabbris@anl.gov>
2020-07-25 20:13:10 -0500

    Updates to devices
    - Fixed phase plates
    - added all slits

--- profile_bluesky/startup/instrument/devices/phaseplates.py
+++ profile_bluesky/startup/instrument/devices/phaseplates.py
@@ -13,21 +13,15 @@ from ophyd import Component, Device, EpicsMotor
 
 class PRDevice(Device):
 
-    x = None
-    y = None
-    th = None
+    def __init__(PV,motorsPVdict,name,**kwargs):
+        super().__init__(prefix=PV, name=name, **kwargs)
 
-    def __init__(PV,motorsPVdict,**kwargs):
-        super().__init__(PV,**kwargs)
-        self.make_motors(motorsPVdict)
+        for key in motorsPVDict.keys():
+            setattr(self,key,EpicsMotor(PV+motorsDict[key], name=key,
+                                        labels=('motor','phase retarders'),parent=self))
 
-    def make_motors(self,motorsPVdict):
-        for key in motorsPVdict.keys():
-            setattr(self,key,Component(EpicsMotor,motorsPVdict[key],name=key,
-                                       labels=('motor','phase retarder')))
-
-pr1 = PRDevice('4idb',{'x':'m10','y':'m11','th':'m13'},name='pr1')
-pr2 = PRDevice('4idb',{'x':'m15','y':'m16','th':'m18'},name='pr2')
-pr3 = PRDevice('4idb',{'x':'m19','y':'m20','th':'m21'},name='pr3')
+pr1 = PRDevice('4idb:',{'x':'m10','y':'m11','th':'m13'},'pr1')
+pr2 = PRDevice('4idb:',{'x':'m15','y':'m16','th':'m18'},'pr2')
+pr3 = PRDevice('4idb:',{'x':'m19','y':'m20','th':'m21'},'pr3')
 
 # TODO: add other stuff to pr's, like lock-in PVS, and screen position.
--- profile_bluesky/startup/instrument/devices/slits.py
+++ profile_bluesky/startup/instrument/devices/slits.py
@@ -2,7 +2,7 @@
 Slits
 """
 
-__all__ = ['enslt']
+__all__ = ['wbslt,''enslt','inslt','grdslt','detslt','magslt']
 
 from instrument.session_logs import logger
 logger.info(__file__)
@@ -11,12 +11,23 @@ from ophyd import Device, EpicsMotor,EpicsSignal
 
 class SlitDevice(Device):
 
-    def __init__(self,PV,motorsDict,signalsDict,name,**kwargs):
+    def __init__(self,PV,motorsDict,signalsDict,name,slitnum = None,**kwargs):
         super().__init__(prefix=PV, name=name, **kwargs)
 
         for key in motorsDict.keys():
             setattr(self,key,EpicsMotor(PV+motorsDict[key], name=key,
                                         labels=('motor','slits'),parent=self))
+
+        if signalsDict is None:
+            if type(slitnum) != int:
+                raise TypeError('slitnum has to be an integer!')
+            else:
+                prefix = 'Slit{}'.format(slitnum)
+                signalsDict = {'vcen': [prefix+'Vt2.D',prefix+'Vcenter'],
+                               'vsize':[prefix+'Vt2.C',prefix+'Vsize'],
+                               'hcen': [prefix+'Ht2.D',prefix+'Hcenter'],
+                               'hsize':[prefix+'Ht2.C',prefix+'Hsize']}
+
         for key in signalsDict.keys():
             setattr(self,key,EpicsSignal(PV+signalsDict[key][0],
                                          write_pv = PV+signalsDict[key][1],
@@ -26,10 +37,37 @@ class SlitDevice(Device):
 # TODO: Do we need to change the "name" of these devices? Depends on how it
 # is saved in the database.
 
+# TODO: There are PVs for the limits of the EpicsSignals, should I add them?
+# Slit4Vsize.DRVH (high lim), .DRVL is low lim. Not clear how to set the PV name
+# of the limits.
+
+
+## White beam slit ##
+wbslt = SlitDevice('4idb:',
+                   {'top': 'm25','bot':'m26','out':'m27','inb':'m28'},
+                   None,'wbslt',slitnum=1)
+
+## Entrance slit ##
 enslt = SlitDevice('4iddx:',
                    {'top': 'm1','bot':'m2','out':'m3','inb':'m4'},
-                   {'vcen': ['Slit1Vt2.D','Slit1Vcenter'],
-                    'vsize':['Slit1Vt2.C','Slit1Vsize'],
-                    'hcen': ['Slit1Ht2.D','Slit1Hcenter'],
-                    'hsize':['Slit1Hsize','Slit1Hsize']},
-                   'enslt')
+                   None,'enslt',slitnum=1)
+
+## 8C incident slit ##
+inslt = SlitDevice('4iddx:',
+                   {'top': 'm5','bot':'m6','out':'m7','inb':'m11'},
+                   None,'inslt',slitnum=2)
+
+## 2th guard slit ##
+grdslt = SlitDevice('4iddx:',
+                    {'top': 'm21','bot':'m22','out':'m23','inb':'m24'},
+                    None,'grdslt',slitnum=3)
+
+## 2th detector slit ##
+detslt = SlitDevice('4iddx:',
+                    {'top': 'm25','bot':'m26','out':'m27','inb':'m28'},
+                    None,'detslt',slitnum=4)
+
+## 2th detector slit ##
+magslt = SlitDevice('4iddx:',
+                    {'top': 'm29','bot':'m30','out':'m31','inb':'m32'},
+                    None,'magslt',slitnum=5)
