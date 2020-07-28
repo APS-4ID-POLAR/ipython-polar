6a33ad86b2ee5e41a721c3769333d072a79d5a58
gfabbris <gfabbris@anl.gov>
2020-07-27 22:40:12 -0500

    Attempt to setup mono

--- profile_bluesky/startup/instrument/devices/monochromator.py
+++ profile_bluesky/startup/instrument/devices/monochromator.py
@@ -9,15 +9,17 @@ __all__ = [
 from ..session_logs import logger
 logger.info(__file__)
 
+from apsutils.devices import KohzuSeqCtl_Monochromator as Kohzu
 from ophyd import Component, Device, EpicsMotor
 
-class Monochromator(Device):
-    th = EpicsMotor('4idb:m1', name='mono', labels=('motor',))  # Kohzu Theta # home_slew_rate=0
-    y = EpicsMotor('4idb:m2', name='mon_y', labels=('motor',))  # Kohzu Y2
-    z = EpicsMotor('4idb:m3', name='mon_z', labels=('motor',))  # Kohzu Y2  # Kohzu Z2
+class Monochromator(Kohzu):
+
+    #th = EpicsMotor('4idb:m1', name='mono', labels=('motor',))  # Kohzu Theta # home_slew_rate=0
+    #y = EpicsMotor('4idb:m2', name='mon_y', labels=('motor',))  # Kohzu Y2
+    #z = EpicsMotor('4idb:m3', name='mon_z', labels=('motor',))  # Kohzu Y2  # Kohzu Z2
     thf = EpicsMotor('4idb:m4', name='mon_thf', labels=('motor',))  # Kohzu Th2f
     chi = EpicsMotor('4idb:m5', name='mon_chi', labels=('motor',))  # Kohzu Chi
 
-mono = Monochromator(name='mono')
+mono = Monochromator('4idb:', name='monochromator')
 
-# TODO: ~~~ REPLACE THIS BY KOHZU FROM APSUTILS ~~~
+# TODO: Should we add control over other stuff?
