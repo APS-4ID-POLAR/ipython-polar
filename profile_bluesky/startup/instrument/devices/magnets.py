37326e472eca2ed3e5bbab0ac6cddac6d51e331e
gfabbris <gfabbris@anl.gov>
2020-08-06 15:43:15 -0500

    Initial setup of lakeshores

--- profile_bluesky/startup/instrument/devices/magnets.py
+++ profile_bluesky/startup/instrument/devices/magnets.py
@@ -9,10 +9,13 @@ __all__ = [
 from ..session_logs import logger
 logger.info(__file__)
 
-from ophyd import Component, Device, EpicsMotor
+from ophyd import Component, Device, EpicsMotor, FormattedComponent
+from .lakeshore import LS336_LoopMore
 
 ## Magnet and sample motors ##
 class Magnet6T(Device):
+
+    ## Motors ##
     tabth = Component(EpicsMotor,'m53', labels=('motor','6T magnet'))  # 4T Mag Th
     tabx = Component(EpicsMotor,'m49', labels=('motor','6T magnet'))  # 4T MagTab X
     taby = Component(EpicsMotor,'m50', labels=('motor','6T magnet'))  # 4T MagTab Y
@@ -23,6 +26,10 @@ class Magnet6T(Device):
 
     sampy = Component(EpicsMotor,'m63', labels=('motor','6T magnet'))  # CIATRA
     sampth = Component(EpicsMotor,'m58', labels=('motor','6T magnet'))  # CIA ROT
+
+    Tvaporizer = FormattedComponent(LS336_LoopMore, "4idd:LS336:TC3", loop_number=1)
+    Tsample = FormattedComponent(LS336_LoopMore, "4idd:LS336:TC3", loop_number=2)
+
 
 mag6t = Magnet6T('4iddx:',name='6T magnet')
 
