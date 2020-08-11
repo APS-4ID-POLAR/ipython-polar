54ff4b72c91d6bb45f9203fde4bf6c0b0dd27beb
gfabbris <gfabbris@anl.gov>
2020-08-11 16:09:46 -0500

    Modified lakeshore setup
    Changed the lakeshore setup to use PVPositoner. As I understand this method is prefered as it allows for using temperature as a positioner in Bluesky. For instance, one could do: bp.mv(temp,100)

--- profile_bluesky/startup/instrument/devices/lakeshores.py
+++ profile_bluesky/startup/instrument/devices/lakeshores.py
@@ -12,7 +12,7 @@ from apstools.synApps.asyn import AsynRecord
 from bluesky import plan_stubs as bps
 from ophyd import Component, Device, DeviceStatus, Signal
 from ophyd import EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV
-from ophyd import FormattedComponent
+from ophyd import FormattedComponent,PVPositioner,SignalRO
 import time
 
 # TODO: fixes bug in apstools/synApps/asyn.py
@@ -20,17 +20,27 @@ class MyAsynRecord(AsynRecord):
     binary_output_maxlength = Component(EpicsSignal, ".OMAX")
 
 ####### LAKESHORE 336 #########
-class LS336_LoopBase(ProcessController):
-    """
-    One control loop on the LS336 temperature controller
+class DoneSignal(SignalRO):
+    def get(self,**kwargs):
+        readback = self.parent.readback.get()
+        setpoint = self.parent.setpoint.get()
+        tolerance = self.parent.tolerance
 
-    Each control loop is a separate process controller.
-    """
+        if abs(readback-setpoint) <= tolerance:
+            self._readback = 1
+        else:
+            self._readback = 0
 
-    signal = FormattedComponent(EpicsSignalRO, "{self.prefix}IN{self.loop_number}")
-    temperature = signal
+        return self._readback
 
-    target = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}OUT{self.loop_number}:SP")
+class L336_LoopControl(PVPositioner):
+
+    readback = FormattedComponent(EpicsSignalRO, "{self.prefix}IN{self.loop_number}")
+    setpoint = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}OUT{self.loop_number}:SP")
+
+    done = Component(DoneSignal,value=0)
+    done_value = 1
+
     units = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}IN{self.loop_number}:Units", kind="omitted")
 
     loop_name = FormattedComponent(EpicsSignalRO, "{self.prefix}IN{self.loop_number}:Name_RBV")
@@ -39,19 +49,6 @@ class LS336_LoopBase(ProcessController):
     manual = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}OUT{self.loop_number}:MOUT")
     mode = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}OUT{self.loop_number}:Mode")
 
-    def __init__(self, *args, loop_number=None, **kwargs):
-        self.controller_name = f"Lakeshore 336 Controller Loop {loop_number}"
-        self.loop_number = loop_number
-        super().__init__(*args, **kwargs)
-
-    def get(self, *args, **kwargs):
-        return self.signal.get(*args, **kwargs)
-
-class LS336_LoopMore(LS336_LoopBase):
-    """
-    Additional controls for loop1 and loop2: heater and pid
-    """
-    # only on loops 1 & 2
     heater = FormattedComponent(EpicsSignalRO, "{self.prefix}HTR{self.loop_number}")
     heater_range = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}HTR{self.loop_number}:Range")
 
@@ -61,15 +58,48 @@ class LS336_LoopMore(LS336_LoopBase):
     ramp_rate = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}RampR{self.loop_number}")
     ramp_on = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}OnRamp{self.loop_number}")
 
+    def __init__(self, *args,loop_number=None,timeout=60*60*10,**kwargs):
+        self.loop_number = loop_number
+        super().__init__(*args,timeout=timeout,**kwargs)
+        self.settle_time = 0
+
+    @property
+    def egu(self):
+        return self.units.get()
+
+    def get(self, *args, **kwargs):
+        return self.readback.get(*args, **kwargs)
+
+    def set(self,*args,**kwargs):
+        return self.move(*args,wait=True,timeout=self._timeout,**kwargs)
+
+    def put(self,*args,**kwargs):
+        return self.move(*args,wait=False,timeout=self._timeout,**kwargs)
+
+    def stop(self,*,success=False):
+        self.put(self.get())
+        super().stop(success=success)
+
+class LS336_LoopRO(Device):
+    """
+    Additional controls for loop1 and loop2: heater and pid
+    """
+    readback = FormattedComponent(EpicsSignalRO, "{self.prefix}IN{self.loop_number}")
+    units = FormattedComponent(EpicsSignalWithRBV, "{self.prefix}IN{self.loop_number}:Units", kind="omitted")
+    loop_name = FormattedComponent(EpicsSignalRO, "{self.prefix}IN{self.loop_number}:Name_RBV")
+
+    def __init__(self, *args,loop_number=None,**kwargs):
+        self.loop_number = loop_number
+        super().__init__(*args,**kwargs)
 
 class LS336Device(Device):
     """
     support for Lakeshore 336 temperature controller
     """
-    loop1 = FormattedComponent(LS336_LoopMore, "{self.prefix}", loop_number=1)
-    loop2 = FormattedComponent(LS336_LoopMore, "{self.prefix}", loop_number=2)
-    loop3 = FormattedComponent(LS336_LoopBase, "{self.prefix}", loop_number=3)
-    loop4 = FormattedComponent(LS336_LoopBase, "{self.prefix}", loop_number=4)
+    loop1 = FormattedComponent(LS336_LoopControl, "{self.prefix}", loop_number=1)
+    loop2 = FormattedComponent(LS336_LoopControl, "{self.prefix}", loop_number=2)
+    loop3 = FormattedComponent(LS336_LoopRO, "{self.prefix}", loop_number=3)
+    loop4 = FormattedComponent(LS336_LoopRO, "{self.prefix}", loop_number=4)
 
     # same names as apstools.synApps._common.EpicsRecordDeviceCommonAll
     scanning_rate = Component(EpicsSignal, "read.SCAN")
@@ -82,10 +112,15 @@ class LS336Device(Device):
 lakeshore_336 = LS336Device("4idd:LS336:TC3:", name="lakeshore 360", labels=["Lakeshore"])
 
 ####### LAKESHORE 340 #########
-class LS340_LoopBase(ProcessController):
 
-    target = FormattedComponent(EpicsSignal, "{self.prefix}SP{self.loop_number}",
-                                write_pv='{self.prefix}wr_SP{self.loop_number}')
+class L340_LoopBase(PVPositioner):
+
+    readback = Component(Signal,value=0)
+    setpoint = FormattedComponent(EpicsSignal, "{self.prefix}SP{self.loop_number}",
+                                    write_pv='{self.prefix}wr_SP{self.loop_number}')
+
+    done = Component(DoneSignal,value=0)
+    done_value = 1
 
     # TODO: Check if this PV exist...
     units = Component(Signal,value='Kelvin')
@@ -107,25 +142,39 @@ class LS340_LoopBase(ProcessController):
                                    write_pv='{self.prefix}setRamp{self.loop_number}.BB')
     ramp_on = FormattedComponent(EpicsSignal, "{self.prefix}Ramp{self.loop_number}_on")
 
-    def __init__(self, *args,loop_number=None, **kwargs):
-        self.controller_name = f"Lakeshore 340 Controller"
+    def __init__(self, *args,loop_number=None,timeout=60*60*10,**kwargs):
         self.loop_number = loop_number
-        super().__init__(*args, **kwargs)
+        super().__init__(*args,timeout=timeout,**kwargs)
+        self.settle_time = 0
+
+    @property
+    def egu(self):
+        return self.units.get()
 
     def get(self, *args, **kwargs):
-        return self.signal.get(*args, **kwargs)
+        return self.readback.get(*args, **kwargs)
 
-def LS340_LoopSample(LS340_LoopBase):
-    signal = FormattedComponent(EpicsSignalRO, "{self.prefix}Sample")
-    temperature = signal
-    sensor = FormattedComponent(EpicsSignal, "{self.prefix}Spl_sel")
+    def set(self,*args,**kwargs):
+        return self.move(*args,wait=True,timeout=self._timeout,**kwargs)
 
-def LS340_LoopControl(LS340_LoopBase):
-    signal = FormattedComponent(EpicsSignalRO, "{self.prefix}Control")
-    temperature = signal
+    def put(self,*args,**kwargs):
+        return self.move(*args,wait=False,timeout=self._timeout,**kwargs)
+
+    def stop(self,*,success=False):
+        self.put(self.get())
+        super().stop(success=success)
+
+class LS340_LoopControl(LS340_LoopBase):
+
+    readback = FormattedComponent(EpicsSignalRO, "{self.prefix}Control")
     sensor = FormattedComponent(EpicsSignal, "{self.prefix}Ctl_sel")
 
-def LS340Device(Device):
+class LS340_LoopSample(LS340_LoopBase):
+
+    readback = FormattedComponent(EpicsSignalRO, "{self.prefix}Sample")
+    sensor = FormattedComponent(EpicsSignal, "{self.prefix}Spl_sel")
+
+class LS340Device(Device):
 
     control = FormattedComponent(LS340_LoopControl, "{self.prefix}", loop_number=1)
     sample = FormattedComponent(LS340_LoopSample, "{self.prefix}", loop_number=2)
@@ -146,5 +195,14 @@ def LS340Device(Device):
 
 lakeshore_340lt = LS340Device('4idd:LS340:TC1:',name="lakeshore 340 - low temperature",
                               labels=("Lakeshore"))
+
 lakeshore_340ht = LS340Device('4idd:LS340:TC2:',name="lakeshore 340 - high temperature",
                               labels=("Lakeshore"))
+
+# TODO: PVPositioner has a .settle_time that should wait after reached temperature.
+# Need to think about the best way to implement it, should it be defaulted to zero,
+# and modified in a plan?
+
+# TODO: Test how to implement "watcher" options. It's under MoveStatus, so it may not
+# be trivial. Perhaps this would be done through callbacks? I suspect the best effort
+# callback will already add something.
