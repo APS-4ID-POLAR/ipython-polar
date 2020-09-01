1e76f9694b2a4106d2b07ef8ffc3abe70f1839ec
gfabbris <gfabbris@anl.gov>
2020-09-01 18:31:09 -0500

    Fix typos in flat and torroidal mirror

--- profile_bluesky/startup/instrument/devices/flat_mirror.py
+++ profile_bluesky/startup/instrument/devices/flat_mirror.py
@@ -17,16 +17,16 @@ class FlatMirror(Device):
 
     y_upstream   = Component(EpicsMotor,'m39',labels=('motor','mirrors'))
     y_downstream = Component(EpicsMotor,'m40',labels=('motor','mirrors'))
-    y = Component(EpicsSignal,'M2t2.D',write_pv=':M2avg',
+    y = Component(EpicsSignal,'M2t2.D',write_pv='M2avg',
                           labels=('mirrors'))
-    angle = Component(EpicsSignal,'M2t2.C',write_pv=':M2angl',
+    angle = Component(EpicsSignal,'M2t2.C',write_pv='M2angl',
                         labels=('mirrors'))
 
     x_upstream   = Component(EpicsMotor,'m37',labels=('motor','mirrors'))
     x_downstream = Component(EpicsMotor,'m38',labels=('motor','mirrors'))
-    x = Component(EpicsSignal,'M2xt2.D',write_pv=':M1xavg',
+    x = Component(EpicsSignal,'M2xt2.D',write_pv='M1xavg',
                           labels=('mirrors'))
-    x_angle = Component(EpicsSignal,'M1xt2.C',write_pv=':M1xangl',
+    x_angle = Component(EpicsSignal,'M1xt2.C',write_pv='M1xangl',
                         labels=('mirrors'))
 
 flat_mirror = FlatMirror('4idb:',name='flat mirror')
--- profile_bluesky/startup/instrument/devices/toroidal_mirror.py
+++ profile_bluesky/startup/instrument/devices/toroidal_mirror.py
@@ -18,17 +18,17 @@ class ToroidalMirror(Device):
 
     y_upstream   = Component(EpicsMotor,'m35',labels=('motor','mirrors'))
     y_downstream = Component(EpicsMotor,'m36',labels=('motor','mirrors'))
-    y = Component(EpicsSignal,'M1t2.D',write_pv=':M1avg',
+    y = Component(EpicsSignal,'M1t2.D',write_pv='M1avg',
                           labels=('motor','mirrors'))
-    angle = Component(EpicsSignal,'M1t2.C',write_pv=':M1angl',
+    angle = Component(EpicsSignal,'M1t2.C',write_pv='M1angl',
                         labels=('motor','mirrors'))
 
 
     x_upstream   = Component(EpicsMotor,'m33',labels=('motor','mirrors'))
     x_downstream = Component(EpicsMotor,'m34',labels=('motor','mirrors'))
-    x = Component(EpicsSignal,'M1xt2.D',write_pv=':M1xavg',
+    x = Component(EpicsSignal,'M1xt2.D',write_pv='M1xavg',
                           labels=('mirrors'))
-    x_angle = Component(EpicsSignal,'M1xt2.C',write_pv=':M1xangl',
+    x_angle = Component(EpicsSignal,'M1xt2.C',write_pv='M1xangl',
                         labels=('mirrors'))
 
     bender = Component(EpicsMotor,'m32',labels=('motor','mirrors'))
