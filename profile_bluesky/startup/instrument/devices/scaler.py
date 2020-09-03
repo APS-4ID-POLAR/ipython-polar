edce313b0712e798a7ba0d8c0b6fa52aed763c91
gfabbris <gfabbris@anl.gov>
2020-09-03 16:36:25 -0500

    Modified ScalerCH to allow for plotting selected channels.

--- profile_bluesky/startup/instrument/devices/scaler.py
+++ profile_bluesky/startup/instrument/devices/scaler.py
@@ -13,14 +13,54 @@ logger.info(__file__)
 
 from ophyd.scaler import ScalerCH
 from ..framework import sd
+from ophyd import Kind
 
-scalerd = ScalerCH('4id:scaler1', name='scalerd', labels=('detectors','counters'))
+
+class LocalScalerCH(ScalerCH):
+    
+    @property
+    def hints(self):
+        fields = []
+        for component in scalerd.channels.component_names: 
+            channel = getattr(scalerd.channels,component) 
+            if channel.kind.value in [5,7]:
+                if len(channel.s.name) > 0:
+                    fields.append(channel.s.name)
+        return {'fields':fields}
+    
+    def select_plot_channels(self,chan_names):
+        
+        self.match_names()
+        name_map = {}
+        for s in self.channels.component_names:
+            scaler_channel = getattr(self.channels, s)
+            nm = scaler_channel.s.name  # as defined in self.match_names()
+            if len(nm) > 0:
+                name_map[nm] = s
+
+        if chan_names is None:
+            chan_names = name_map.keys()
+
+        for ch in name_map.keys():
+            try:
+                if ch in chan_names: 
+                    getattr(self.channels,name_map[ch]).kind = Kind.hinted
+                else:
+                    getattr(self.channels,name_map[ch]).kind = Kind.normal
+            except KeyError:
+                raise RuntimeError("The channel {} is not configured "
+                                   "on the scaler.  The named channels are "
+                                   "{}".format(ch, tuple(name_map)))
+
+scalerd = LocalScalerCH('4id:scaler1', name='scalerd', labels=('detectors','counters'))
 scalerd.select_channels(None)
+scalerd.select_plot_channels(None)
 sd.baseline.append(scalerd)
 
-scalerb = ScalerCH('4idb:scaler1', name='scalerb', labels=('detectors','counters'))
+scalerb = LocalScalerCH('4idb:scaler1', name='scalerb', labels=('detectors','counters'))
 scalerb.channels.chan01.chname.set('Time_b')
 scalerb.select_channels(None)
+scalerb.select_plot_channels(None)
 sd.baseline.append(scalerb)
 
 # TODO: name the other channels, watch out for python keywords such as del!
