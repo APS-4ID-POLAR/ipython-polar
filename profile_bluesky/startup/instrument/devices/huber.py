7fe090436c3faed82387045900efbc05b4ac30cc
gfabbris <gfabbris@anl.gov>
2020-10-02 16:08:55 -0500

    Initial diffractometer setup

--- profile_bluesky/startup/instrument/devices/huber.py
+++ profile_bluesky/startup/instrument/devices/huber.py
@@ -4,8 +4,7 @@ Diffractometer motors
 """
 
 __all__ = [
-    'cryo',
-    'huber'
+    'fourc',
     ]
 
 # add these imports
@@ -17,35 +16,40 @@ __all__ = [
 from ..session_logs import logger
 logger.info(__file__)
 
-from ophyd import Component,EpicsMotor,MotorBundle
+
+import gi
+gi.require_version('Hkl', '5.0')
+# MUST come before `import hkl`
+from hkl.diffract import E4CV
+from ophyd import Component
+from ophyd import PseudoSingle
+from ophyd import EpicsSignal, EpicsSignalRO, EpicsMotor, Signal
 from bluesky.suspenders import SuspendBoolLow
-from ophyd import EpicsSignal
+import pint
+from apstools.diffractometer import Constraint
+from apstools.diffractometer import DiffractometerMixin
 from ..framework import RE
 from ..framework import sd
 
-## Cryo carrier ##
-class CryoStage(MotorBundle):
-    x = Component(EpicsMotor, 'm14', labels=('motor', 'cryo'))  # Cryo X
-    y = Component(EpicsMotor, 'm15', labels=('motor', 'cryo'))  # Cryo Y
-    z = Component(EpicsMotor, 'm16', labels=('motor', 'cryo'))  # Cryo Z
+class FourCircleDiffractometer(DiffractometerMixin, E4CV):
+    """
+    E4CV: kappa diffractometer in 4-circle geometry with energy.
 
-cryo = CryoStage(prefix='4iddx:',name='cryo')
-sd.baseline.append(cryo)
+    4-ID-D setup.
+    """
+    # HKL and 4C motors
+    h = Component(PseudoSingle, '', labels=("hkl", "fourc"), kind="hinted")
+    k = Component(PseudoSingle, '', labels=("hkl", "fourc"), kind="hinted")
+    l = Component(PseudoSingle, '', labels=("hkl", "fourc"), kind="hinted")
 
-## 8c rotations ##
-class Diffractometer(MotorBundle):
-    th = Component(EpicsMotor,'m65', labels=('motor','diffractometer'))  # Theta # slop=2
-    tth = Component(EpicsMotor,'m66', labels=('motor','diffractometer'))  # Two Theta
-    phi = Component(EpicsMotor,'m68', labels=('motor','diffractometer'))  # Phi
-    chi = Component(EpicsMotor,'m67', labels=('motor','diffractometer'))  # Chi
-    bth = Component(EpicsMotor,'m69', labels=('motor','diffractometer'))  # Base Th
-    btth = Component(EpicsMotor,'m70', labels=('motor','diffractometer'))  # Base tth
-    ath = Component(EpicsMotor,'m77', labels=('motor','diffractometer'))  # Ana Theta
-    achi = Component(EpicsMotor,'m79', labels=('motor','diffractometer'))  # Ana Chi
-    atth = Component(EpicsMotor,'m78', labels=('motor','diffractometer'))  # Ana 2Theta
-    x = Component(EpicsMotor,'m18', labels=('motor','diffractometer'))  # 8C horiz
-    y = Component(EpicsMotor,'m17', labels=('motor','diffractometer'))  # 8C verical
-
+    omega = Component(EpicsMotor, 'm65', labels=("motor", "fourc"),
+                      kind="hinted")
+    chi = Component(EpicsMotor, 'm67', labels=("motor", "fourc"),
+                    kind="hinted")
+    phi = Component(EpicsMotor, 'm68', labels=("motor", "fourc"),
+                    kind="hinted")
+    tth = Component(EpicsMotor, 'm66', labels=("motor", "fourc"),
+                    kind="hinted")
 
     th_tth_min = Component(EpicsSignal, "userCalc1.C",
                            labels=('diffractometer', 'limits'),
@@ -54,20 +58,55 @@ class Diffractometer(MotorBundle):
                               labels=('diffractometer', 'limits'),
                               kind='config')
 
+    # Cryo carrier
+    x = Component(EpicsMotor, 'm14', labels=('motor', 'fourc'))  # Cryo X
+    y = Component(EpicsMotor, 'm15', labels=('motor', 'fourc'))  # Cryo Y
+    z = Component(EpicsMotor, 'm16', labels=('motor', 'fourc'))  # Cryo Z
 
-huber = Diffractometer(prefix='4iddx:',name='huber')
-sus = SuspendBoolLow(huber.th_tth_permit)
+    # Base motors
+    baseth = Component(EpicsMotor, 'm69', labels=('motor', 'fourc'))
+    basetth = Component(EpicsMotor, 'm70', labels=('motor', 'fourc'))
+
+    tablex = Component(EpicsMotor, 'm18', labels=('motor', 'fourc'))
+    tabley = Component(EpicsMotor, 'm17', labels=('motor', 'fourc'))
+
+    # Analyzer motors
+    ath = Component(EpicsMotor, 'm77', labels=('motor', 'fourc'))
+    achi = Component(EpicsMotor, 'm79', labels=('motor', 'fourc'))
+    atth = Component(EpicsMotor, 'm78', labels=('motor', 'fourc'))
+
+    # Energy
+    energy = Component(EpicsSignalRO, "4idb:BraggERdbkAO", kind='omitted')
+    # energy_EGU = Component(EpicsSignal, "optics:energy.EGU")
+    energy_update_calc = Component(Signal, value=1)
+    energy_offset = Component(Signal, value=0)
+
+    def _energy_changed(self, value=None, **kwargs):
+        '''
+        Callback indicating that the energy signal was updated.
+        '''
+        if not self.connected:
+            logger.warning("%s not fully connected, %s.calc.energy not updated",
+                           self.name, self.name)
+            return
+        if self.energy_update_calc.get() in (1, "Yes", "locked", "OK"):
+            # energy_offset has same units as energy
+            local_energy = value + self.energy_offset.get()
+
+            # TODO: I think we don't need this. It's always keV.
+            # # either get units from control system
+            # units = self.energy_EGU.get()
+            # # or define as a constant here
+            # # units = "eV"
+            #
+            # keV = pint.Quantity(local_energy, units).to("keV")
+            # logger.debug("setting %s.calc.energy = %f (keV)", self.name,
+            #              keV.magnitude)
+            self._calc.energy = local_energy
+            self._update_position()
+
+
+fourc = FourCircleDiffractometer(prefix='4iddx:', name='fourc')
+sus = SuspendBoolLow(fourc.th_tth_permit)
 RE.install_suspender(sus)
-
-sd.baseline.append(huber)
-
-# TODO: look at todo folder. Use hklpy when setting these up, so that we
-#       can create fourc and sixc
-
-# TODO: discuss the extend of which we want to add classes like the one below
-#class Diffractometer(Device):
-#   cryo = Component(CryoStage)
-#   scaler = ScalerCH( ... )
-#   filters =
-#   magnets =
-#diffractometer = Diffractometer(name="diffractometer")
+sd.baseline.append(fourc)
