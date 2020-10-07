03fee51eedb7794145cd73c0da9ee01896d443ac
gfabbris <gfabbris@anl.gov>
2020-10-07 17:01:26 -0500

    create calibrate_energy function

--- profile_bluesky/startup/instrument/devices/monochromator.py
+++ profile_bluesky/startup/instrument/devices/monochromator.py
@@ -13,23 +13,41 @@ from apstools.devices import KohzuSeqCtl_Monochromator
 from ophyd import Component, EpicsSignalRO, EpicsMotor, EpicsSignal
 from ..framework import sd
 
+
 class Monochromator(KohzuSeqCtl_Monochromator):
 
     y1 = None
 
-    x2 = Component(EpicsMotor,'m6',labels=('motor','monochromator'))
-    y2 = Component(EpicsSignalRO,'KohzuYRdbkAI',labels=('motor','monochromator'))   # Kohzu Y2
-    z2 = Component(EpicsSignalRO,'KohzuZRdbkAI',labels=('motor','monochromator'))  # Kohzu Z2
+    x2 = Component(EpicsMotor, 'm6', labels=('motor', 'monochromator'))
+    y2 = Component(EpicsSignalRO, 'KohzuYRdbkAI',
+                   labels=('motor', 'monochromator'))
+    z2 = Component(EpicsSignalRO, 'KohzuZRdbkAI',
+                   labels=('motor', 'monochromator'))
 
-    thf2 = Component(EpicsMotor,'m4',labels=('motor','monochromator'))  # Kohzu Th2f
-    chi2 = Component(EpicsMotor,'m5', labels=('motor','monochromator'))  # Kohzu Chi
+    thf2 = Component(EpicsMotor, 'm4', labels=('motor', 'monochromator'))
+    chi2 = Component(EpicsMotor, 'm5', labels=('motor', 'monochromator'))
 
-    table_x = Component(EpicsMotor,'m7',labels=('motor','monochromator'))
-    table_y = Component(EpicsMotor,'m8',labels=('motor','monochromator'))
+    table_x = Component(EpicsMotor, 'm7', labels=('motor', 'monochromator'))
+    table_y = Component(EpicsMotor, 'm8', labels=('motor', 'monochromator'))
 
     energy = Component(EpicsSignal, "BraggERdbkAO", write_pv="BraggEAO",
-                       kind='hinted', put_complete = True)
+                       kind='hinted', put_complete=True)
+
+    def calibrate_energy(self, value):
+        """Calibrate the mono energy.
+
+        Sets the current monochromator position to a new value.
+
+        Parameters
+        ----------
+        value: float
+            New energy for the current monochromator position.
+        """
+        self.use_set.put('Set')
+        self.energy.put(value)
+        self.use_set.put('Use')
+
 
 mono = Monochromator('4idb:', name='monochromator')
-mono.stage_sigs['mode'] = 1 #Ensure that mono is in auto before moving.
+mono.stage_sigs['mode'] = 1  # Ensure that mono is in auto before moving.
 sd.baseline.append(mono)
