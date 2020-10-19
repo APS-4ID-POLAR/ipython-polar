bf3d856f52eb68e03c06c52f7cd91ac082f32ef6
gfabbris <gfabbris@anl.gov>
2020-10-19 15:48:39 -0500

    Fix typos in ruby_motors

--- profile_bluesky/startup/instrument/devices/ruby_motors.py
+++ profile_bluesky/startup/instrument/devices/ruby_motors.py
@@ -10,10 +10,10 @@ from ophyd import Component, MotorBundle, EpicsMotor
 from ..framework import sd
 
 class RubyMotors(MotorBundle):
-  focus = Component(EpicsMotor,':m37',labels=('motor','ruby')
-  y = Component(EpicsMotor,':m38',labels=('motor','ruby')
-  z = Component(EpicsMotor,':m39',labels=('motor','ruby')
-  zoom = Component(EpicsMotor,':m40',labels=('motor','ruby')
+  focus = Component(EpicsMotor,'m37',labels=('motor','ruby'))
+  y = Component(EpicsMotor,'m38',labels=('motor','ruby'))
+  z = Component(EpicsMotor,'m39',labels=('motor','ruby'))
+  zoom = Component(EpicsMotor,'m40',labels=('motor','ruby'))
 
 
 ruby_mot = RubyMotors('4iddx:', name='ruby_motors')
