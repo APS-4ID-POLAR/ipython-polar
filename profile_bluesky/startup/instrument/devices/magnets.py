d429c2201bbff7a81f991a24d473adf49a119a58
gfabbris <gfabbris@anl.gov>
2020-11-26 10:56:56 -0600

    Tweaks and typos

--- profile_bluesky/startup/instrument/devices/magnets.py
+++ profile_bluesky/startup/instrument/devices/magnets.py
@@ -1,5 +1,5 @@
 """
-Magnet motors
+Magnets
 """
 
 __all__ = [
@@ -16,59 +16,72 @@ from bluesky.plan_stubs import mv, sleep
 from ..utils import local_rd
 
 class AMIZones(Device):
-    high_field = Component(EpicsSignal,"Field.VAL",write_pv="FieldSet")
-    ramp_rate = Component(EpicsSignal,"Rate",write_pv="RateSet")
+    high_field = Component(EpicsSignal, "Field.VAL", write_pv="FieldSet")
+    ramp_rate = Component(EpicsSignal, "Rate", write_pv="RateSet")
 
 class AMIController(PVPositioner):
 
     # position
-    readback = Component(EpicsSignalRO, "Field",auto_monitor=True,
-                         kind=Kind.hinted)
+    readback = Component(EpicsSignalRO, "Field", auto_monitor=True,
+                         kind=Kind.hinted, labels=('ami_controller', 'magnet'))
 
-    setpoint = Component(EpicsSignal, "PField",
-                         write_pv="PField:Wrt.A",auto_monitor=True,
-                         kind=Kind.hinted)
+    setpoint = Component(EpicsSignal, "PField", write_pv="PField:Wrt.A",
+                         auto_monitor=True, kind=Kind.hinted,
+                         labels=('ami_controller', 'magnet'))
 
-    current = Component(EpicsSignalRO, "Current",auto_monitor=True,
-                        kind=Kind.omitted)
+    current = Component(EpicsSignalRO, "Current", auto_monitor=True,
+                        kind=Kind.normal, labels=('ami_controller', 'magnet'))
 
-    voltage = Component(EpicsSignalRO, "Voltage",auto_monitor=True,
-                        kind=Kind.omitted)
+    voltage = Component(EpicsSignalRO, "Voltage", auto_monitor=True,
+                        kind=Kind.normal, labels=('ami_controller', 'magnet'))
 
-    supply_current = Component(EpicsSignalRO, "CurrentSP",
-                               auto_monitor=True, kind=Kind.omitted)
-    #status
+    supply_current = Component(EpicsSignalRO, "CurrentSP", auto_monitor=True,
+                               kind=Kind.normal,
+                               labels=('ami_controller', 'magnet'))
+    # status
     magnet_status = Component(EpicsSignalRO, "StateInt.A", auto_monitor=True,
-                              kind=Kind.config)
-    
+                              kind=Kind.config,
+                              labels=('ami_controller', 'magnet'))
+
     done = magnet_status
-    
     done_value = 2
-    # TODO: Need to check what are the status available!!
 
     # configuration
-    units = Component(EpicsSignalRO, "FieldUnits.SVAL", kind=Kind.config)
+    units = Component(EpicsSignalRO, "FieldUnits.SVAL", kind=Kind.config,
+                      labels=('ami_controller', 'magnet'))
 
-    field_limit = Component(EpicsSignalRO,"Field:Limit",kind=Kind.config)
-    current_limit = Component(EpicsSignalRO, "Curr:Limit.VAL",kind=Kind.config)
-    voltage_limit = Component(EpicsSignalRO, "Volt:Limit.VAL",kind=Kind.config)
+    field_limit = Component(EpicsSignalRO, "Field:Limit", kind=Kind.config,
+                            labels=('ami_controller', 'magnet'))
+    current_limit = Component(EpicsSignalRO, "Curr:Limit.VAL",
+                              kind=Kind.config,
+                              labels=('ami_controller', 'magnet'))
+    voltage_limit = Component(EpicsSignalRO, "Volt:Limit.VAL",
+                              kind=Kind.config,
+                              labels=('ami_controller', 'magnet'))
 
     switch_heater = Component(EpicsSignal, "PSOnOff", string=True,
-                              kind=Kind.config)
+                              kind=Kind.config,
+                              labels=('ami_controller', 'magnet'))
 
-    zone_1 = Component(AMIZones,"RampR1:",kind=Kind.config)
-    zone_2 = Component(AMIZones,"RampR2:",kind=Kind.config)
-    zone_3 = Component(AMIZones,"RampR3:",kind=Kind.config)
-    ramp_units = Component(EpicsSignalRO,"RampR:Units.SVAL",kind=Kind.config)
+    zone_1 = Component(AMIZones, "RampR1:", kind=Kind.config,
+                       labels=('ami_controller', 'magnet'))
+    zone_2 = Component(AMIZones, "RampR2:", kind=Kind.config,
+                       labels=('ami_controller', 'magnet'))
+    zone_3 = Component(AMIZones, "RampR3:", kind=Kind.config,
+                       labels=('ami_controller', 'magnet'))
+    ramp_units = Component(EpicsSignalRO, "RampR:Units.SVAL", kind=Kind.config,
+                           labels=('ami_controller', 'magnet'))
 
     # Buttons
-    ramp_button = Component(EpicsSignal,"Ramp.PROC",kind=Kind.omitted)
-    pause_button = Component(EpicsSignal,"Pause.PROC",kind=Kind.omitted)
-    zero_button = Component(EpicsSignal,"Zero.PROC",kind=Kind.omitted,
+    ramp_button = Component(EpicsSignal, "Ramp.PROC", kind=Kind.omitted,
+                            put_complete=True)
+    pause_button = Component(EpicsSignal, "Pause.PROC", kind=Kind.omitted,
+                             put_complete=True)
+    zero_button = Component(EpicsSignal, "Zero.PROC", kind=Kind.omitted,
                             put_complete=True)
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args,**kwargs)
+        super().__init__(*args, **kwargs)
         self._settle_time = 0
 
     @property
@@ -76,7 +89,7 @@ class AMIController(PVPositioner):
         return self._settle_time
 
     @settle_time.setter
-    def settle_time(self,value):
+    def settle_time(self, value):
         if value < 0:
             raise ValueError('Settle value needs to be >= 0.')
         else:
@@ -86,7 +99,7 @@ class AMIController(PVPositioner):
     def egu(self):
         return self.units.get(as_string=True)
 
-    def stop(self,*,success=False):
+    def stop(self, *, success=False):
         if success is False:
             self.pause_button.put(1)
         super().stop(success=success)
@@ -95,40 +108,35 @@ class AMIController(PVPositioner):
         self.pause_button.put(1)
 
     @done.sub_value
-    def _move_changed(self,**kwargs):
+    def _move_changed(self, **kwargs):
         super()._move_changed(**kwargs)
-        
-        
 
-# Magnet and sample motors #
+
+
+# Magnet and sample motors
 class Magnet6T(Device):
 
     # Motors #
-    tabth = Component(EpicsMotor, 'm53', labels=('motor'))  # 4T Mag Th
-    tabx = Component(EpicsMotor, 'm49', labels=('motor'))  # 4T MagTab X
-    taby = Component(EpicsMotor, 'm50', labels=('motor'))    # 4T MagTab Y
+    tabth = Component(EpicsMotor, 'm53', labels=('motor', 'magnet'))
+    tabx = Component(EpicsMotor, 'm49', labels=('motor', 'magnet'))
+    taby = Component(EpicsMotor, 'm50', labels=('motor', 'magnet'))
 
-    tabth2 = Component(EpicsMotor,'m56', labels=('motor'))  # AMIMagnetPhi
-    tabz2 = Component(EpicsMotor,'m51', labels=('motor'))  # AMIMagnetZ
-    tabx2 = Component(EpicsMotor,'m52', labels=('motor'))  # AMIMagenetX
+    tabth2 = Component(EpicsMotor, 'm56', labels=('motor', 'magnet'))
+    tabz2 = Component(EpicsMotor, 'm51', labels=('motor', 'magnet'))
+    tabx2 = Component(EpicsMotor, 'm52', labels=('motor', 'magnet'))
 
-    sampy = Component(EpicsMotor,'m63', labels=('motor'))  # CIATRA
-    sampth = Component(EpicsMotor,'m58', labels=('motor'))  # CIA ROT
-
-    # Temperatures
-    # TODO: Do we want these here?
-    Tvaporizer = None
-    Tsample = None
+    sampy = Component(EpicsMotor, 'm63', labels=('motor', 'magnet'))
+    sampth = Component(EpicsMotor, 'm58', labels=('motor', 'magnet'))
 
     # Magnetic field controller
-    field = Component(AMIController,'4idd:AMI430:',add_prefix='')
-    
+    field = Component(AMIController, '4idd:AMI430:', add_prefix='')
+
     def _stage_magnet(self):
-        
+
         if self.field.switch_heater.get() != 'On':
-            
+
             yield from mv(self.field.ramp_button, 1)
-            
+
             while True:
                 supply = yield from local_rd(self.field.supply_current)
                 target = yield from local_rd(self.field.current)
@@ -136,53 +144,45 @@ class Magnet6T(Device):
                     yield from sleep(1)
                 else:
                     break
-                
-            
+
             yield from mv(self.field.switch_heater, 'On')
-            
+
             while True:
                 _status = yield from local_rd(self.field.magnet_status)
-            
+
                 if _status != 3:
                     yield from sleep(1)
                 else:
                     break
-                
+
             yield from mv(self.field.ramp_button, 1)
-            
-            
+
     def _unstage_magnet(self):
-        
+
         while True:
             voltage = yield from local_rd(self.field.voltage)
             if abs(voltage) > 0.01:
                 yield from sleep(1)
             else:
                 break
-            
+
         yield from mv(self.field.switch_heater, 'Off')
-        
+
         while True:
             _status = yield from local_rd(self.field.magnet_status)
-            
-            if _status not in [2,3]:
+
+            if _status not in [2, 3]:
                 yield from sleep(1)
             else:
                 break
-        
+
         yield from mv(self.field.zero_button, 1)
-    
-    def move_field(self,target):
-        
+
+    def move_field(self, target):
+
         yield from self._stage_magnet()
-        yield from mv(self.field,target)
+        yield from mv(self.field, target)
         yield from self._unstage_magnet()
-        
 
 
-mag6t = Magnet6T('4iddx:',name='6T magnet')
-
-
-
-# TODO: Is it ok to add the lakeshores here?
-# TODO: should we add the magnet field controls here?
+mag6t = Magnet6T('4iddx:', name='magnet_6T')
