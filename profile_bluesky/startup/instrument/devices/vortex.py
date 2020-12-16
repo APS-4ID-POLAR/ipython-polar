0c911c31359b0b9a28b04a590609155d5903e10e
YChoi <68574132+CrI3@users.noreply.github.com>
2020-12-16 14:06:46 -0600

    testing1

--- profile_bluesky/startup/instrument/devices/vortex.py
+++ profile_bluesky/startup/instrument/devices/vortex.py
@@ -3,3 +3,5 @@
 # https://github.com/NSLS-II-HXN/ipython_ophyd/blob/master/profile_collection/startup/21-xspress3.py
 # https://github.com/NSLS-II-HXN/hxntools/blob/master/hxntools/detectors/
 # https://github.com/NSLS-II-CHX/profile_collection/blob/master/startup/98-xspress3.py
+
+# ...test saving
