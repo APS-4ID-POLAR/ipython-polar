17fad269ad7bb4b938574e8d6fe0545b8e0e13d7
YChoi <68574132+CrI3@users.noreply.github.com>
2020-12-21 16:12:41 -0600

    planning

--- profile_bluesky/startup/instrument/devices/vortex.py
+++ profile_bluesky/startup/instrument/devices/vortex.py
@@ -4,9 +4,7 @@
 # https://github.com/NSLS-II-HXN/hxntools/blob/master/hxntools/detectors/
 # https://github.com/NSLS-II-CHX/profile_collection/blob/master/startup/98-xspress3.py
 
-
-# ...test saving
-# CHX using different electronics (zebra)
+# 
 
 from ophyd import Device, Component, EpicsSignal
 
@@ -16,17 +14,58 @@ class Xspress3Vortex(Device, num_ch = 4):
     AcquireTime_button = Component(EpicsSignal, 'det1:AcquireTime', trigger_value=1, kind='omitted')                         
     Erase_button = Component(EpicsSignal, 'det1:Erase', trigger_value=1, kind='omitted')
     NumImage_button = Component(EpicsSignal, 'det1:NumImages', trigger_value=1, kind='omitted')                         
-    TriggerMode_button = = Component(EpicsSignal, 'det1:TriggerMode', trigger_value=1, kind='omitted')
+    TriggerMode_button = Component(EpicsSignal, 'det1:TriggerMode', trigger_value=1, kind='omitted')
+    AbsEdge = 'IrL3'  # select roi energy range, make a dict for edge:emission line...
     #TriggerMode, 1:internal, 3: TTL veto only
+    AcuireMode = 'step'  #step: trigger once and read roi pvs only, frame: 
+    hdf5 = Component(EpicsSignal, 'HDF5:',
+        # HDF5 saving useful when NumImage is large, 
+        #S4QX4:HDF1:Capture # 0/1: done/capture, this needs to be 1 before acquiring to save hdf files
+        #S4QX4:HDF1:FilePath  # /home/xspress3/data
+        #S4QX4:HDF1:FileName  # file prefix, "data1"
+        #S4QX4:HDF1:FileTemplate  # filename format, %s%s%d.hdf5
+        )
 
-    # HDF5 saving useful when NumImage is large, 
-    #S4QX4:HDF1:Capture # 0/1: done/capture, this needs to be 1 before acquiring to save hdf files
-    #S4QX4:HDF1:FilePath  # /home/xspress3/data
-    #S4QX4:HDF1:FileName  # file prefix, "data1"
-    #S4QX4:HDF1:FileTemplate  # filename format, %s%s%d.hdf5
 
-#str1 = '%sMCA%dROI:1:Total_RBV' % (self.prefix, 1) # roi 
+    def __init__(self, prefix, *, configuration_attrs=None, read_attrs=None,**kwargs):
+        pass
+        # initialize hdf5 folers
+        # set default rois 
 
 
-vortex1 = Xspress3Vortex("XSP3_1Chan:", 1)     # single channel unit
-vortex4 = Xspress3Vortex("S4QX4:", 4)          # 4 channel unit
+    def stage(self, *args, **kwargs): # need to separate, hdf5 saving or none.
+        pass
+        # if frame mode: hdf5: 
+        #   S4QX4:HDF1:Capture # 0/1: done/capture, this needs to be 1 before acquiring to save hdf files
+        #   put default det1:NumImages, det1:AcquireTime
+        # else:
+        #   det1:NumImages=1, det1:AcquireTime
+
+
+    def unstage(self, *args, **kwargs):
+        pass
+
+
+    def set_roi(self, *arg):
+        pass
+        # make a function Edge2Emission(AbsEdge) --> returns primary emission energy
+        # 1st argument for roi1, 2nd for roi2...
+        # 'S4QX4:MCA1ROI:1:Total_RBV'  # roi1 of channel 1
+        # 'S4QX4:MCA1ROI:2:Total_RBV'  # roi1 of channel 2
+
+    def trigger(self):
+        pass
+        # after triggering, check and return det1:DetectorState_RBV         
+        # after DetectorState_RBV, update pvs
+
+    # need to add something like below when checking status.
+    def check_value(*, old_value, value, **kwargs):
+        """Return True when the acquisition is complete, False otherwise."""
+        end_states= ("Aborted", "Idle")
+        return value in end_states and old_value not in end_states
+
+# two detectors using xspress3 ioc and hitachi vortex hardware
+#vortex1 = Xspress3Vortex("XSP3_1Chan:", 1)     # single channel unit
+#vortex4 = Xspress3Vortex("S4QX4:", 4)          # 4 channel unit
+
+
