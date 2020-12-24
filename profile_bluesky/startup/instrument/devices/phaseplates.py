c4cf001e00c81518ce5b331fb9dd10553057026b
gfabbris <gfabbris@anl.gov>
2020-12-23 18:00:55 -0600

    testing with PR

--- profile_bluesky/startup/instrument/devices/phaseplates.py
+++ profile_bluesky/startup/instrument/devices/phaseplates.py
@@ -1,13 +1,6 @@
 """
-Phase retarders
+Phase retarders.
 """
-
-__all__ = [
-    'pr1', 'pr2', 'pr3', 'pr_setup',
-    ]
-
-from ..session_logs import logger
-logger.info(__file__)
 
 from ..framework import sd
 from ophyd import Device, EpicsMotor
@@ -20,7 +13,14 @@ from numpy import arcsin, pi
 # This is here because PRDevice.select_pr has a micron symbol that utf-8
 # cannot read. See: https://github.com/bluesky/ophyd/issues/930
 from epics import utils3
+from ..session_logs import logger
+
+logger.info(__file__)
 utils3.EPICS_STR_ENCODING = "latin-1"
+
+__all__ = [
+    'pr1', 'pr2', 'pr3', 'pr_setup',
+    ]
 
 
 class TrackingSignal(Signal):
@@ -36,6 +36,20 @@ class TrackingSignal(Signal):
         if type(value) != bool:
             msg = 'tracking is boolean, it can only be True or False.'
             raise ValueError(msg)
+
+
+class ThetaMotor(EpicsMotor):
+
+    def update_theta(self, old_value=None, value=None, **kwargs):
+
+        if not value:
+            # lamb in angstroms
+            lamb = speed_of_light*Planck*6.241509e15*1e10/value
+
+            # theta in degrees
+            theta = arcsin(lamb/2/self.d_spacing.get())*180./pi
+
+            self.set(theta)
 
 
 class PRPzt(Device):
@@ -93,8 +107,10 @@ class PRDeviceBase(Device):
     y = FormattedComponent(EpicsMotor, '{self.prefix}:{_motorsDict[y]}',
                            labels=('motor', 'phase retarders'))
 
-    th = FormattedComponent(EpicsMotor, '{self.prefix}:{_motorsDict[th]}',
+    th = FormattedComponent(ThetaMotor, '{self.prefix}:{_motorsDict[th]}',
                             labels=('motor', 'phase retarders'))
+
+    energy = Component(Signal, value=10.0, labels=('phase retardders',))
 
     tracking = Component(TrackingSignal, value=False)
     d_spacing = Component(Signal, value=0, kind='config')
@@ -104,6 +120,9 @@ class PRDeviceBase(Device):
     def __init__(self, PV, name, motorsDict, **kwargs):
         self._motorsDict = motorsDict
         super().__init__(prefix=PV, name=name, **kwargs)
+
+        self.energy.subscribe(self.th.update_theta,
+                              sub_type=self.energy.SUB_VALUE)
 
     def set_energy(self, energy):
         # energy in keV!
@@ -134,7 +153,7 @@ class PRDevice(PRDeviceBase):
     def _set_d_spacing(self, value=None, **kwargs):
         if isinstance(value, int):
             value = self.select_pr.get()
-        
+
         spacing_dictionary = {'111': 2.0595, '220': 1.26118}
         plane = value.split('(')[1].split(')')[0]
         self.d_spacing.put(spacing_dictionary[plane])
@@ -171,7 +190,8 @@ class PRSetup():
                             _positioner = pr.th
                         else:
                             while True:
-                                method = input('\tUse motor or PZT? (motor/pzt): ')
+                                method = input('\tUse motor or PZT? \
+                                    (motor/pzt): ')
                                 if method.lower() == 'motor':
                                     _positioner = pr.th
                                     break
@@ -179,10 +199,12 @@ class PRSetup():
                                     _positioner = pr.pzt.localDC
                                     break
                                 else:
-                                    print("Only motor or pzt are acceptable answers.")
+                                    print("Only motor or pzt are acceptable\
+                                        answers.")
                             while True:
                                 try:
-                                    _center = float(input('\tPZT center (in microns): '))
+                                    _center = float(input('\tPZT center (in\
+                                        microns): '))
                                     _positioner.parent.center.put(_center)
                                     break
                                 except ValueError:
@@ -191,8 +213,10 @@ class PRSetup():
 
                         while True:
                             try:
-                                _offset = float(input('\tOffset (in degrees): '))
-                                _positioner.parent.update_offset_degrees(_offset)
+                                _offset = float(input('\tOffset (in degrees): \
+                                    '))
+                                _positioner.parent.update_offset_degrees(
+                                    _offset)
                                 break
                             except ValueError:
                                 print('Must be a number.')
@@ -204,7 +228,8 @@ class PRSetup():
                         print("Only yes or no are acceptable answers.")
 
             else:
-                print('\tYou already selected {} to oscillate.'.format(_positioner.name))
+                print('\tYou already selected {} to oscillate.'.format(
+                    _positioner.name))
 
         self.positioner = _positioner
 
