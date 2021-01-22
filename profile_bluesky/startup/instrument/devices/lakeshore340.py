f69c6bc941f809b05b52daa4d8ff5b533f16e9b4
Gilberto Fabbris <gfabbris@anl.gov>
2021-01-22 11:55:22 -0600

    Fix unsubscribing. Add staging to move.

--- profile_bluesky/startup/instrument/devices/lakeshore340.py
+++ profile_bluesky/startup/instrument/devices/lakeshore340.py
@@ -2,14 +2,14 @@
 Lakeshore temperature controllers
 """
 
-from instrument.session_logs import logger
-logger.info(__file__)
-
 from apstools.synApps.asyn import AsynRecord
-from ophyd import Component, Device, Signal
+from ophyd import Component, Device, Signal, Staged
 from ophyd import EpicsSignal, EpicsSignalRO
 from ophyd import FormattedComponent, PVPositioner
 from ..utils import DoneSignal, TrackingSignal
+
+from instrument.session_logs import logger
+logger.info(__file__)
 
 
 class LS340_LoopBase(PVPositioner):
@@ -93,17 +93,26 @@ class LS340_LoopBase(PVPositioner):
         super()._move_changed(**kwargs)
 
     def move(self, *args, **kwargs):
+
+        # TODO: This is an area that may be problematic. Need to test at
+        # beamline when doing scan and when just moving.
+
+        # This will apply only when we are moving, not scanning. In the later,
+        # the stage/unstage will run before/after the scan.
+        if self._staged == Staged.no:
+            self.stage()
+            self.subscribe(self.unstage, event_type=self._SUB_REQ_DONE)
         self.done.put(0)
         return super().move(*args, **kwargs)
 
     def stage(self):
-        self.readback.subscribe(self.done.get)
-        self.setpoint.subscribe(self.done.get)
+        self.readback.subscribe(self.done.get, event_type='value')
+        self.setpoint.subscribe(self.done.get, event_type='value')
         super().stage()
 
-    def unstage(self):
-        self.readback.unsubscribe(self.done.get)
-        self.setpoint.unsubscribe(self.done.get)
+    def unstage(self, **kwargs):
+        self.readback.clear_subs(self.done.get)
+        self.setpoint.clear_subs(self.done.get)
         super().unstage()
 
 
@@ -144,17 +153,13 @@ class LS340Device(Device):
 
     serial = Component(AsynRecord, "serial", kind="omitted")
 
+    # This must be modified either here, or before using auto_heater.
+    _auto_ranges = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # TODO: I don't know why this has to be done, otherwise it gets hinted.
         self.control.readback.kind = "normal"
-        self._auto_ranges = {
-            '10 mA': (0, 10),
-            '33 mA': None,
-            '100 mA': (10, 30),
-            '333 mA': None,
-            '1 A': (30, 305),
-        }
 
     @auto_heater.sub_value
     def _subscribe_auto_heater(self, value=None, **kwargs):
@@ -162,7 +167,7 @@ class LS340Device(Device):
             self.control.setpointRO.subscribe(self._switch_heater,
                                               event_type='value')
         else:
-            self.control.setpointRO.unsubscribe(self._switch_heater)
+            self.control.setpointRO.clear_subs(self._switch_heater)
 
     def _switch_heater(self, value=None, **kwargs):
         # TODO: Find a better way to do this, perhaps with an array?
@@ -170,3 +175,25 @@ class LS340Device(Device):
             if _temp_range:
                 if _temp_range[0] < value <= _temp_range[1]:
                     self.heater_range.put(_heater_range)
+
+    @property
+    def auto_ranges(self):
+        return self._auto_ranges
+
+    @auto_ranges.setter
+    def auto_ranges(self, value):
+        if not isinstance(value, dict):
+            raise TypeError('auto_ranges must be a dictionary.')
+
+        for _heater_range, _temp_range in value:
+            if _heater_range not in self.heater_range.enum_strs:
+                raise ValueError("The input dictionary keys must be one of "
+                                 f"these: {self.heater_range.enum_strs}, but "
+                                 f"{_heater_range} was entered.")
+
+            if _temp_range is not None and len(_temp_range) != 2:
+                raise ValueError(f"The value {_temp_range} is invalid! It "
+                                 "must be either None or an iterable with two "
+                                 "items.")
+
+            self._auto_ranges = value
