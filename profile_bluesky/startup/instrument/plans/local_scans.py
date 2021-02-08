3ce0886ff0cf19ec885628c85addbe9a25cedddd
Gilberto Fabbris <gfabbris@anl.gov>
2021-02-08 15:47:48 -0600

    Add qxscan based on list_scan

--- profile_bluesky/startup/instrument/plans/local_scans.py
+++ profile_bluesky/startup/instrument/plans/local_scans.py
@@ -2,25 +2,36 @@
 Modifed bluesky scans
 """
 
-__all__ = ['lup', 'ascan', 'mv']
+__all__ = ['lup', 'ascan', 'mv', 'qxscan']
 
-from bluesky.plans import rel_scan, scan
+from bluesky.plans import rel_scan, scan, list_scan
 from bluesky.plan_stubs import trigger_and_read, move_per_step
 from bluesky.plan_stubs import mv as bps_mv, rd
 from ..devices import (pr_setup, mag6t, energy, undulator, pr1, pr2, pr3,
-                       scalerd)
+                       scalerd, qxscan_params)
 from .local_preprocessors import (configure_monitor_decorator,
                                   stage_dichro_decorator,
                                   stage_ami_decorator,
                                   energy_scan_decorator)
 from ..utils import local_rd
+from numpy import array
 
 from ..session_logs import logger
 logger.info(__file__)
 
-# TODO: I would like to have something like this, but it cannot just be a list.
-# Something like an imutable list might work.
-# DEFAULT_DETECTORS = [scalerd]
+
+def _collect_extras():
+
+    extras = []
+    und_track = yield from rd(undulator.downstream.tracking)
+    if und_track:
+        extras.append(undulator.downstream.energy)
+    for pr in [pr1, pr2, pr3]:
+        pr_track = yield from rd(pr.tracking)
+        if pr_track:
+            extras.append(pr.th)
+
+    return extras
 
 
 def dichro_steps(detectors, motors, take_reading):
@@ -113,7 +124,7 @@ def lup(*args, monitor=None, detectors=None, lockin=False,
 
     if not detectors:
         detectors = [scalerd]
-    
+
     extras = []
     if energy in args:
         extras = yield from _collect_extras()
@@ -176,10 +187,10 @@ def ascan(*args, monitor=None, detectors=None, lockin=False,
         magnet = True
     else:
         magnet = False
-        
+
     if not detectors:
         detectors = [scalerd]
-    
+
     extras = []
     if energy in args:
         extras = yield from _collect_extras()
@@ -192,6 +203,53 @@ def ascan(*args, monitor=None, detectors=None, lockin=False,
         yield from scan(detectors+extras, *args, per_step=per_step, **kwargs)
 
     return (yield from _inner_ascan())
+
+
+def qxscan(edge_energy, monitor=None, detectors=None, lockin=False,
+           dichro=False, **kwargs):
+
+    if not detectors:
+        detectors = [scalerd]
+
+    per_step = one_dichro_step if dichro else None
+
+    # Get energy argument and extras
+    energy_list = yield from local_rd(qxscan_params.energy_list)
+    args = (energy, array(energy_list) + edge_energy)
+
+    extras = []
+    if energy in args:
+        extras = yield from _collect_extras()
+
+    # Setup count time
+    factor_list = yield from local_rd(qxscan_params.factor_list)
+
+    _ct = {}
+    if monitor:
+        if monitor < 0 and detectors != [scalerd]:
+            raise TypeError('monitor < 0 can only be used with scaler.')
+        else:
+            for det in detectors:
+                _ct[det] = abs(monitor)
+                args += (det.preset_monitor, abs(monitor)*array(factor_list))
+    else:
+        for det in detectors:
+            _ct[det] = yield from rd(det.preset_monitor)
+            args += (det.preset_monitor, _ct[det]*array(factor_list))
+
+    @energy_scan_decorator(energy in args, extras)
+    @stage_ami_decorator(mag6t.field in args)
+    @configure_monitor_decorator(monitor)
+    @stage_dichro_decorator(dichro, lockin)
+    def _inner_qxscan():
+        yield from list_scan(detectors+extras, *args, per_step=per_step,
+                             **kwargs)
+
+        # put original times back.
+        for det, preset in _ct.items():
+            yield from mv(det.preset_monitor, preset)
+
+    return (yield from _inner_qxscan())
 
 
 def mv(*args, **kwargs):
@@ -227,17 +285,3 @@ def mv(*args, **kwargs):
         yield from bps_mv(*args, **kwargs)
 
     return (yield from _inner_mv())
-
-
-def _collect_extras():
-    
-    extras = []
-    und_track = yield from rd(undulator.downstream.tracking)
-    if und_track:
-        extras.append(undulator.downstream.energy)
-    for pr in [pr1, pr2, pr3]:
-        pr_track = yield from rd(pr.tracking)
-        if pr_track:
-            extras.append(pr.th)
-    
-    return extras
