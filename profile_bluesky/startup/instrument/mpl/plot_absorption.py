99c0de663261be3720c87f54c0eeb1aaff74e183
Gilberto Fabbris <gfabbris@anl.gov>
2021-02-27 14:49:15 -0600

    Add docstring to plot_absorption

--- profile_bluesky/startup/instrument/mpl/plot_absorption.py
+++ profile_bluesky/startup/instrument/mpl/plot_absorption.py
@@ -12,6 +12,39 @@ __all__ = ['plot_xas', 'plot_dichro', 'plot_lockin']
 
 def plot_xas(scans=-1, positioner=None, detector=None, monitor=None,
              fluo=False, label=None, ax=None):
+    """
+    Plot the absorption spectrum.
+
+    Parameters
+    ----------
+    scans : int, uid or iterable of ints, uid, optional
+        Scan number or uid. If a list of scans is passed, it will average them.
+        Defaults to the last scan.
+    positioner : str, optional
+        Name of the positioner used. If None, it defaults to the monochromator.
+    detector : str, optional
+        Name of the detector used. If None, it defaults to the Ion Ch 5.
+    monitor : str, optional
+        Name of the detector used as monitor. If None, it defaults to the Ion
+        Ch 4.
+    fluo : bool, optional
+        Flag to choose to process as transmission (False) or fluorescence
+        (True).
+    label : str, optional
+        Label for this data. If None, it defaults to `scans`. This is useful
+        when comparing multiple samples.
+    ax : matplotlib axes, optional
+        Axes instance to use in the plot. If None, it will produce one.
+
+    Returns
+    -------
+    fig, ax : matplotlib figure and axes instances
+        Useful to reuse or modify this figure.
+
+    See also
+    --------
+    :func:`polartools.absorption.load_multi_xas`
+    """
 
     try:
         scans = list(scans)
@@ -52,36 +85,67 @@ def plot_xas(scans=-1, positioner=None, detector=None, monitor=None,
     return fig, ax
 
 
-def plot_lockin(scans_plus=-1, scans_minus=-1, positioner=None, fluo=False,
+def plot_lockin(scans_plus, scans_minus, positioner=None, fluo=False,
                 title=''):
+    """
+    Plot the XMCD signal measured in lockin.
+
+    If either scans input is None, it will skip it.
+
+    Parameters
+    ----------
+    scans_plus : int, uid or iterable of ints, uid
+        Scan number or uid of scans measured with + magnetic field. If a list
+        of scans is passed, it will average them.
+    scans_minus : int, uid or iterable of ints, uid
+        Scan number or uid of scans measured with - magnetic field. If a list
+        of scans is passed, it will average them.
+    positioner : str, optional
+        Name of the positioner used. If None, it defaults to the monochromator.
+    fluo : bool, optional
+        Flag to choose to process as transmission (False) or fluorescence
+        (True).
+    title : str, optional
+        Title for the plot. By default it will add the scan numbers to it.
+
+    Returns
+    -------
+    fig, ax : matplotlib figure and axes instances
+        Useful to modify this figure.
+
+    See also
+    --------
+    :func:`polartools.absorption.load_multi_lockin`
+    """
 
     fig, axs = plt.subplots(2, 2, figsize=(8, 6))
 
     results = []
     for scans, label in zip([scans_plus, scans_minus], ['plus', 'minus']):
+        if scans is not None:
+            try:
+                scans = list(scans)
+            except TypeError:
+                scans = [scans]
 
-        try:
-            scans = list(scans)
-        except TypeError:
-            scans = [scans]
+            if title != '':
+                title += '\n'
+            title += f'{label}: {scans}'
 
-        if title != '':
-            title += '\n'
-        title += f'{label}: {scans}'
+            results.append(load_multi_lockin(
+                scans, db, positioner=positioner, transmission=not fluo
+                ))
 
-        results.append(load_multi_lockin(
-            scans, db, positioner=positioner, transmission=not fluo
-            ))
+            _plot_one_xmcd(results[-1], axs[0], label)
 
-        _plot_one_xmcd(results[-1], axs[0], label)
+    if len(results) > 1:
+        combined = (
+            results[0][0],  # energy
+            (results[0][3] - results[0][3])/2.,  # xmcd
+            (results[0][3] + results[0][3])/2.,  # artifact
+            )
 
-    combined = (
-        results[0][0],  # energy
-        (results[0][3] - results[0][3])/2.,  # xmcd
-        (results[0][3] + results[0][3])/2.,  # artifact
-        )
-
-    _plot_combined_xmcd(combined, axs[1])
+        _plot_combined_xmcd(combined, axs[1])
 
     plt.suptitle(title)
     plt.tight_layout()
@@ -89,8 +153,44 @@ def plot_lockin(scans_plus=-1, scans_minus=-1, positioner=None, fluo=False,
     return fig, axs
 
 
-def plot_dichro(scans_plus=-1, scans_minus=-1, positioner=None, detector=None,
+def plot_dichro(scans_plus, scans_minus, positioner=None, detector=None,
                 monitor=None, fluo=False, title=''):
+
+    """
+    Plot the XMCD signal measured in lockin.
+
+    If either scans input is None, it will skip it.s
+
+    Parameters
+    ----------
+    scans_plus : int, uid or iterable of ints, uid
+        Scan number or uid of scans measured with + magnetic field. If a list
+        of scans is passed, it will average them.
+    scans_minus : int, uid or iterable of ints, uid
+        Scan number or uid of scans measured with - magnetic field. If a list
+        of scans is passed, it will average them.
+    positioner : str, optional
+        Name of the positioner used. If None, it defaults to the monochromator.
+    detector : str, optional
+        Name of the detector used. If None, it defaults to the Ion Ch 5.
+    monitor : str, optional
+        Name of the detector used as monitor. If None, it defaults to the Ion
+        Ch 4.
+    fluo : bool, optional
+        Flag to choose to process as transmission (False) or fluorescence
+        (True).
+    title : str, optional
+        Title for the plot. By default it will add the scan numbers to it.
+
+    Returns
+    -------
+    fig, ax : matplotlib figure and axes instances
+        Useful to modify this figure.
+
+    See also
+    --------
+    :func:`polartools.absorption.load_multi_dichro`
+    """
 
     if detector is None:
         detector = []
@@ -108,30 +208,31 @@ def plot_dichro(scans_plus=-1, scans_minus=-1, positioner=None, detector=None,
 
     results = []
     for scans, label in zip([scans_plus, scans_minus], ['plus', 'minus']):
+        if scans is not None:
+            try:
+                scans = list(scans)
+            except TypeError:
+                scans = [scans]
 
-        try:
-            scans = list(scans)
-        except TypeError:
-            scans = [scans]
+            if title != '':
+                title += '\n'
+            title += f'{label}: {scans}\n'
 
-        if title != '':
-            title += '\n'
-        title += f'{label}: {scans}\n'
+            results.append(load_multi_dichro(
+                scans, db, positioner=positioner, detector=detector,
+                monitor=monitor, transmission=not fluo
+                ))
 
-        results.append(load_multi_dichro(
-            scans, db, positioner=positioner, detector=detector,
-            monitor=monitor, transmission=not fluo
-            ))
+            _plot_one_xmcd(results[-1], axs[0], label)
 
-        _plot_one_xmcd(results[-1], axs[0], label)
+    if len(results) > 1:
+        combined = (
+            results[0][0],  # energy
+            (results[0][3] - results[0][3])/2.,  # xmcd
+            (results[0][3] + results[0][3])/2.,  # artifact
+            )
 
-    combined = (
-        results[0][0],  # energy
-        (results[0][3] - results[0][3])/2.,  # xmcd
-        (results[0][3] + results[0][3])/2.,  # artifact
-        )
-
-    _plot_combined_xmcd(combined, axs[1])
+        _plot_combined_xmcd(combined, axs[1])
 
     plt.suptitle(title)
     plt.tight_layout()
