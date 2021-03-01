d0e1bbf2ba72fb5860fd4dde8c3a1f55362562bb
Gilberto Fabbris <gfabbris@anl.gov>
2021-03-01 09:19:22 -0600

    add docstring

--- profile_bluesky/startup/instrument/plans/center_maximum.py
+++ profile_bluesky/startup/instrument/plans/center_maximum.py
@@ -9,7 +9,20 @@ __all__ = ['maxi', 'cen']
 # TODO: read the positioner from hints? Would need a way to convert the name
 # into the actual python object.
 def cen(positioner, detector=None):
+    """
+    Plan that moves motor to center of last scan.
 
+    Uses the position found by the
+    `bluesky.callbacks.best_effort.BestEffortCallback().peaks`.
+
+    Parameters
+    ----------
+    positioner : ophyd instance
+        Device to be moved to center.
+    detector : str, optional
+        Ophyd instance name of the detector used to center. This is only needed
+        if the scan had more than one hinted detector.
+    """
     if detector is None:
         if len(peaks['cen'].keys()) > 1:
             raise TypeError("You need to provide a detector name if more than "
@@ -33,7 +46,20 @@ def cen(positioner, detector=None):
 
 
 def maxi(positioner, detector=None):
+    """
+    Plan that moves motor to the maximum of last scan.
 
+    Uses the position found by the
+    `bluesky.callbacks.best_effort.BestEffortCallback().peaks`.
+
+    Parameters
+    ----------
+    positioner : ophyd instance
+        Device to be moved to center.
+    detector : str, optional
+        Ophyd instance name of the detector used to center. This is only needed
+        if the scan had more than one hinted detector.
+    """
     if detector is None:
         if len(peaks['cen'].keys()) > 1:
             raise TypeError("You need to provide a detector name if more than "
