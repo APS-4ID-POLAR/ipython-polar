c652d8a3876fb222f72823eb46f932814ff0c3e7
Gilberto Fabbris <gfabbris@anl.gov>
2021-03-01 16:41:51 -0600

    dichro_plot reads monitor/detector from hints

--- profile_bluesky/startup/instrument/callbacks/dichro_plot.py
+++ profile_bluesky/startup/instrument/callbacks/dichro_plot.py
@@ -13,27 +13,39 @@ from ..session_logs import logger
 logger.info(__file__)
 
 
-def xanes(monitor, detector):
-    
+def xanes(monitor, detector, fluo):
     if array(detector).size < 4:
         return array(detector).mean()
-    
+
     rng = 4*(array(detector).size//4)
-    absorption = log(array(monitor)[:rng] / array(detector)[:rng]).reshape(-1, 4)
-    
-#    absorption = log(array(monitor) / array(detector)).reshape(-1, 4)
+
+    if fluo:
+        absorption = (
+            (array(detector)[:rng] / array(monitor)[:rng]).reshape(-1, 4)
+        )
+    else:
+        absorption = (
+            log(array(monitor)[:rng] / array(detector)[:rng]).reshape(-1, 4)
+        )
+
     return absorption.mean(axis=1)
 
 
-def xmcd(monitor, detector):
-    
+def xmcd(monitor, detector, fluo):
     if array(detector).size < 4:
         return 0
-    
+
     rng = 4*(array(detector).size//4)
-    absorption = log(array(monitor)[:rng] / array(detector)[:rng]).reshape(-1, 4)
-    
-#    absorption = log(array(monitor) / array(detector)).reshape(-1, 4)
+
+    if fluo:
+        absorption = (
+            (array(detector)[:rng] / array(monitor)[:rng]).reshape(-1, 4)
+        )
+    else:
+        absorption = (
+            log(array(monitor)[:rng] / array(detector)[:rng]).reshape(-1, 4)
+        )
+
     return (absorption[:, [0, 3]].mean(axis=1) -
             absorption[:, [1, 2]].mean(axis=1))
 
@@ -41,17 +53,17 @@ def xmcd(monitor, detector):
 def downsampled(x):
     if array(x).size < 4:
         return array(x).mean()
-
     rng = 4*(array(x).size//4)
     return array(x[:rng]).reshape(-1, 4).mean(axis=1)
 
 
 class AutoDichroPlot(AutoPlotter):
-    def __init__(self, monitor="Ion Ch 4", detector="Ion Ch 5"):
+    def __init__(self, monitor='Ion Ch 4', detector='Ion Ch 5', fluo=True):
         super().__init__()
         self._x_to_lines = {}  # map x variable to (xanes_lines, xmcd_lines)
         self._monitor = monitor
         self._detector = detector
+        self._fluo = fluo
 
     @property
     def monitor(self):
@@ -60,6 +72,10 @@ class AutoDichroPlot(AutoPlotter):
     @property
     def detector(self):
         return self._detector
+
+    @property
+    def fluo(self):
+        return self._fluo
 
     def handle_new_stream(self, run, stream_name):
         if stream_name != "primary":
@@ -70,11 +86,20 @@ class AutoDichroPlot(AutoPlotter):
         scan_type = run.metadata["start"]["hints"].get('scan_type', None)
         if scan_type != 'dichro':
             return
-        
+
         # Detect x variable from hints in metadata.
         first_scan_dimension = run.metadata["start"]["hints"]["dimensions"][0]
         scanning_fields, _ = first_scan_dimension
         x = scanning_fields[0]
+
+        # Collect monitor and detector
+        if "monitor" in run.metadata["start"]["hints"].keys():
+            self._monitor = run.metadata["start"]["hints"]["monitor"]
+        # TODO: This will only use the first detector, but maybe could
+        # expand to all detectors.
+        if "detectors" in run.metadata["start"]["hints"].keys():
+            self._detector = run.metadata["start"]["hints"]["detectors"][0]
+
         # If we already have a figure for this x, reuse it (over-plot).
         try:
             (xanes_lines, xmcd_lines) = self._x_to_lines[x]
@@ -89,13 +114,15 @@ class AutoDichroPlot(AutoPlotter):
             xanes_lines = Lines(
                 x=lambda primary: downsampled(primary[x]),
                 ys=[lambda primary: xanes(primary[self._monitor],
-                                          primary[self._detector])],
+                                          primary[self._detector],
+                                          self.fluo)],
                 axes=xanes_axes,
             )
             xmcd_lines = Lines(
                 x=lambda primary: downsampled(primary[x]),
                 ys=[lambda primary: xmcd(primary[self._monitor],
-                                         primary[self._detector])],
+                                         primary[self._detector],
+                                         self.fluo)],
                 axes=xmcd_axes,
             )
             self._x_to_lines[x] = (xanes_lines, xmcd_lines)
