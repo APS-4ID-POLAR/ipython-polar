ae5ef0df78d58ed3663fdd1d51af3a41fb8879ef
Gilberto Fabbris <gfabbris@anl.gov>
2021-03-26 18:08:11 -0500

    utils: remove local_rd

--- profile_bluesky/startup/instrument/utils/__init__.py
+++ profile_bluesky/startup/instrument/utils/__init__.py
@@ -5,7 +5,6 @@ any extra commands or utility functions here
 
 from .done_signal import *
 from .tracking_signal import *
-from .local_rd import *
 from .load_vortex import *
 from .counters_class import *
 from .plot_funcs import *
