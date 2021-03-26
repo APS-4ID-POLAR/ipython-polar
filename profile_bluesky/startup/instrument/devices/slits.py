f2b11478d4ecd3b22179b81b92d5a47d6f38172b
Gilberto Fabbris <gfabbris@anl.gov>
2021-03-26 17:32:52 -0500

    devices: slits change relative pos to PVPositioner

--- profile_bluesky/startup/instrument/devices/slits.py
+++ profile_bluesky/startup/instrument/devices/slits.py
@@ -3,68 +3,97 @@ Slits
 """
 __all__ = ['wbslt', 'enslt', 'inslt', 'grdslt', 'detslt', 'magslt']
 
+from ophyd import (Device, EpicsMotor, EpicsSignal, EpicsSignalRO, Signal,
+                   FormattedComponent, PVPositioner, Component)
+from ..framework import sd
 from ..session_logs import logger
 logger.info(__file__)
 
-from ophyd import Device, EpicsMotor, EpicsSignal, FormattedComponent
-from ..framework import sd
+
+class PVPositionerSoftDone(PVPositioner):
+
+    # positioner
+    setpoint = FormattedComponent(EpicsSignal, "{prefix}{_readback_pv}",
+                                  kind="normal")
+    readback = FormattedComponent(EpicsSignalRO, "{prefix}{_setpoint_pv}",
+                                  kind="hinted", auto_monitor=True)
+    done = Component(Signal, value=True)
+    done_value = True
+
+    tolerance = Component(Signal, value=0.001)
+
+    def cb_readback(self, *args, **kwargs):
+        """
+        Called when readback changes (EPICS CA monitor event).
+        """
+        diff = self.readback.get() - self.setpoint.get()
+        dmov = abs(diff) <= self.tolerance.get()
+        if self.report_dmov_changes.get() and dmov != self.done.get():
+            logger.debug(f"{self.name} reached: {dmov}")
+        self.done.put(dmov)
+
+    def cb_setpoint(self, *args, **kwargs):
+        """
+        Called when setpoint changes (EPICS CA monitor event).
+        When the setpoint is changed, force done=False.  For any move,
+        done must go != done_value, then back to done_value (True).
+        Without this response, a small move (within tolerance) will not return.
+        Next update of readback will compute self.done.
+        """
+        self.done.put(not self.done_value)
+
+    def __init__(self, prefix, *, limits=None, readback_pv="", setpoint_pv="",
+                 name=None, read_attrs=None, configuration_attrs=None,
+                 parent=None, egu="", **kwargs):
+
+        self._setpoint_pv = setpoint_pv
+        self._readback_pv = readback_pv
+
+        super().__init__(prefix=prefix, limits=limits, name=name,
+                         read_attrs=read_attrs,
+                         configuration_attrs=configuration_attrs,
+                         parent=parent, egu=egu, **kwargs)
+
+        self.readback.subscribe(self.cb_readback)
+        self.setpoint.subscribe(self.cb_setpoint)
 
 
 class SlitDevice(Device):
 
     # Setting motors
     top = FormattedComponent(EpicsMotor, '{prefix}:{_motorsDict[top]}',
-                             labels=('motor', 'slit'))
+                             labels=('motors', 'slits'))
 
     bot = FormattedComponent(EpicsMotor, '{prefix}:{_motorsDict[bot]}',
-                             labels=('motor', 'slit'))
+                             labels=('motors', 'slits'))
 
     out = FormattedComponent(EpicsMotor, '{prefix}:{_motorsDict[out]}',
-                             labels=('motor', 'slit'))
+                             labels=('motors', 'slits'))
 
     inb = FormattedComponent(EpicsMotor, '{prefix}:{_motorsDict[inb]}',
-                             labels=('motor', 'slit'))
+                             labels=('motors', 'slits'))
 
     # Setting pseudo positioners
-    vcen = FormattedComponent(EpicsSignal, '{prefix}:{_signalsDict[vcen][0]}',
-                              write_pv='{prefix}:{_signalsDict[vcen][1]}',
-                              labels=('slit',))
+    vcen = FormattedComponent(PVPositionerSoftDone, '{prefix}{slit_prefix}',
+                              readback_pv='Vt2.D', setpoint_pv='Vcenter',
+                              labels=('slits',))
 
-    hcen = FormattedComponent(EpicsSignal, '{prefix}:{_signalsDict[hcen][0]}',
-                              write_pv='{prefix}:{_signalsDict[hcen][1]}',
-                              labels=('slit',))
+    hcen = FormattedComponent(PVPositionerSoftDone, '{prefix}{slit_prefix}',
+                              readback_pv='Vt2.C', setpoint_pv='Vsize',
+                              labels=('slits',))
 
-    vsize = FormattedComponent(EpicsSignal,
-                               '{prefix}:{_signalsDict[vsize][0]}',
-                               write_pv='{prefix}:{_signalsDict[vsize][1]}',
-                               labels=('slit',))
+    vsize = FormattedComponent(PVPositionerSoftDone, '{prefix}{slit_prefix}',
+                               readback_pv='Ht2.D', setpoint_pv='Hcenter',
+                               labels=('slits',))
 
-    hsize = FormattedComponent(EpicsSignal,
-                               '{prefix}:{_signalsDict[hsize][0]}',
-                               write_pv='{prefix}:{_signalsDict[hsize][1]}',
-                               labels=('slit',))
+    hsize = FormattedComponent(PVPositionerSoftDone, '{prefix}{slit_prefix}',
+                               readback_pv='Ht2.C', setpoint_pv='Hsize',
+                               labels=('slits',))
 
-    def __init__(self, PV, name, motorsDict, slitnum=None, signalsDict=None,
-                 **kwargs):
+    def __init__(self, PV, name, motorsDict, slitnum, **kwargs):
 
         self._motorsDict = motorsDict
-
-        if signalsDict is None:
-            if type(slitnum) != int:
-                raise TypeError('If signalsDict is None, then slitnum has to \
-                    be an integer!')
-            else:
-                prefix = 'Slit{}'.format(slitnum)
-                self._signalsDict = {'vcen': [prefix+'Vt2.D',
-                                              prefix+'Vcenter'],
-                                     'vsize': [prefix+'Vt2.C',
-                                               prefix+'Vsize'],
-                                     'hcen': [prefix+'Ht2.D',
-                                              prefix+'Hcenter'],
-                                     'hsize': [prefix+'Ht2.C',
-                                               prefix+'Hsize']}
-        else:
-            self._signalsDict = signalsDict
+        self._slit_prefix = f'Slit{slitnum}'
 
         super().__init__(prefix=PV, name=name, **kwargs)
 
@@ -72,35 +101,35 @@ class SlitDevice(Device):
 # White beam slit
 wbslt = SlitDevice('4idb', 'wbslt',
                    {'top': 'm25', 'bot': 'm26', 'out': 'm27', 'inb': 'm28'},
-                   slitnum=1)
+                   1)
 sd.baseline.append(wbslt)
 
 # Entrance slit
 enslt = SlitDevice('4iddx', 'enslt',
                    {'top': 'm1', 'bot': 'm2', 'out': 'm3', 'inb': 'm4'},
-                   slitnum=1)
+                   1)
 sd.baseline.append(enslt)
 
 # 8C incident slit
 inslt = SlitDevice('4iddx', 'inslt',
                    {'top': 'm5', 'bot': 'm6', 'out': 'm7', 'inb': 'm11'},
-                   slitnum=2)
+                   2)
 sd.baseline.append(inslt)
 
 # 2th guard slit
 grdslt = SlitDevice('4iddx', 'grdslt',
                     {'top': 'm21', 'bot': 'm22', 'out': 'm23', 'inb': 'm24'},
-                    slitnum=3)
+                    3)
 sd.baseline.append(grdslt)
 
 # 2th detector slit
 detslt = SlitDevice('4iddx', 'detslt',
                     {'top': 'm25', 'bot': 'm26', 'out': 'm27', 'inb': 'm28'},
-                    slitnum=4)
+                    4)
 sd.baseline.append(detslt)
 
 # Magnet incident slit
 magslt = SlitDevice('4iddx', 'magslt',
                     {'top': 'm29', 'bot': 'm30', 'out': 'm31', 'inb': 'm32'},
-                    slitnum=5)
+                    5)
 sd.baseline.append(magslt)
