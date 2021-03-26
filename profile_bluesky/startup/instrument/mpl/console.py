50d361483ecaed028bf30760adbc18519b45cb4e
Gilberto Fabbris <gfabbris@anl.gov>
2021-03-26 18:02:48 -0500

    mpl: tweaks

--- profile_bluesky/startup/instrument/mpl/console.py
+++ profile_bluesky/startup/instrument/mpl/console.py
@@ -5,9 +5,8 @@ Configure matplotlib in interactive mode for IPython console
 
 __all__ = ['plt']
 
+import matplotlib.pyplot as plt
 from ..session_logs import logger
 logger.info(__file__)
-
-import matplotlib.pyplot as plt
 
 plt.ion()
--- profile_bluesky/startup/instrument/mpl/notebook.py
+++ profile_bluesky/startup/instrument/mpl/notebook.py
@@ -5,11 +5,10 @@ Configure matplotlib in interactive mode for Jupyter notebook
 
 __all__ = ['plt']
 
-from ..session_logs import logger
-logger.info(__file__)
-
 import matplotlib.pyplot as plt
 from IPython import get_ipython
+from ..session_logs import logger
+logger.info(__file__)
 
 # %matplotlib notebook
 get_ipython().magic('matplotlib notebook')
