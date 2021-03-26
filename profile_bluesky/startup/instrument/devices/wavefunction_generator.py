c5214e2583e2e7eb9a02da865cc15c16bc0012e3
Gilberto Fabbris <gfabbris@anl.gov>
2021-03-26 17:54:16 -0500

    devices: wavefunc gen tweaks

--- profile_bluesky/startup/instrument/devices/wavefunction_generator.py
+++ profile_bluesky/startup/instrument/devices/wavefunction_generator.py
@@ -4,28 +4,27 @@ Wavefunction generator.
 
 __all__ = ['wavefunc_gen']
 
-
+from ophyd import Device, EpicsSignal, Component
+from ..framework import sd
 from ..session_logs import logger
 logger.info(__file__)
-
-from ophyd import Device, EpicsSignal, Kind, Component
-from ..framework import sd
 
 
 class SRS340(Device):
 
     frequency = Component(EpicsSignal, 'FREQ.VAL', write_pv='SetFREQ.A',
-                          kind=Kind.config, labels=('phase retarders'))
+                          kind='config')
 
     amplitude = Component(EpicsSignal, 'AMPL.VAL', write_pv='SetAMPL.A',
-                          kind=Kind.config, labels=('phase retarders'))
+                          kind='config')
 
     offset = Component(EpicsSignal, 'OFFS.VAL', write_pv='SetOFFS.A',
-                       kind=Kind.config, labels=('phase retarders'))
+                       kind='config')
 
     function = Component(EpicsSignal, 'FUNC.SVAL', write_pv='FUNCs.VAL',
-                         kind=Kind.config, labels=('phase retarders'))
+                         kind='config')
 
 
-wavefunc_gen = SRS340('4idd:SRS340:1:', name='wavefunction_generator')
+wavefunc_gen = SRS340('4idd:SRS340:1:', name='wavefunction_generator',
+                      labels=('phase retarders',))
 sd.baseline.append(wavefunc_gen)
