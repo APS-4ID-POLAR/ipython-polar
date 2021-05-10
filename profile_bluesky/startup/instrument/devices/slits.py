109266bd58debcac6f376b2b7712a635f1de034e
Gilberto Fabbris <gfabbris@anl.gov>
2021-05-10 18:23:22 -0500

    update slits

--- profile_bluesky/startup/instrument/devices/slits.py
+++ profile_bluesky/startup/instrument/devices/slits.py
@@ -3,60 +3,11 @@ Slits
 """
 __all__ = ['wbslt', 'enslt', 'inslt', 'grdslt', 'detslt', 'magslt']
 
-from ophyd import (Device, EpicsMotor, EpicsSignal, EpicsSignalRO, Signal,
-                   FormattedComponent, PVPositioner, Component)
+from ophyd import Device, FormattedComponent, EpicsMotor
+from .util_components import PVPositionerSoftDone
 from ..framework import sd
 from ..session_logs import logger
 logger.info(__file__)
-
-
-class PVPositionerSoftDone(PVPositioner):
-
-    # positioner
-    readback = FormattedComponent(EpicsSignalRO, "{prefix}{_readback_pv}",
-                                  kind="hinted", auto_monitor=True)
-    setpoint = FormattedComponent(EpicsSignal, "{prefix}{_setpoint_pv}",
-                                  kind="normal")
-    done = Component(Signal, value=True)
-    done_value = True
-
-    tolerance = Component(Signal, value=0.001)
-    report_dmov_changes = Component(Signal, value=False, kind="omitted")
-
-    def cb_readback(self, *args, **kwargs):
-        """
-        Called when readback changes (EPICS CA monitor event).
-        """
-        diff = self.readback.get() - self.setpoint.get()
-        dmov = abs(diff) <= self.tolerance.get()
-        if self.report_dmov_changes.get() and dmov != self.done.get():
-            logger.debug(f"{self.name} reached: {dmov}")
-        self.done.put(dmov)
-
-    def cb_setpoint(self, *args, **kwargs):
-        """
-        Called when setpoint changes (EPICS CA monitor event).
-        When the setpoint is changed, force done=False.  For any move,
-        done must go != done_value, then back to done_value (True).
-        Without this response, a small move (within tolerance) will not return.
-        Next update of readback will compute self.done.
-        """
-        self.done.put(not self.done_value)
-
-    def __init__(self, prefix, *, limits=None, readback_pv="", setpoint_pv="",
-                 name=None, read_attrs=None, configuration_attrs=None,
-                 parent=None, egu="", **kwargs):
-
-        self._setpoint_pv = setpoint_pv
-        self._readback_pv = readback_pv
-
-        super().__init__(prefix=prefix, limits=limits, name=name,
-                         read_attrs=read_attrs,
-                         configuration_attrs=configuration_attrs,
-                         parent=parent, egu=egu, **kwargs)
-
-        self.readback.subscribe(self.cb_readback)
-        self.setpoint.subscribe(self.cb_setpoint)
 
 
 class SlitDevice(Device):
