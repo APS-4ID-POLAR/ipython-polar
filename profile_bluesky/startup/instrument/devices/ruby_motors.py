9f42cb24d2c736119305d3de87b3edc4c1f664ea
Gilberto Fabbris <gfabbris@anl.gov>
2021-05-10 18:23:15 -0500

    update DAC in ruby_motors

--- profile_bluesky/startup/instrument/devices/ruby_motors.py
+++ profile_bluesky/startup/instrument/devices/ruby_motors.py
@@ -4,46 +4,26 @@ Ruby spectrometer motors and controls.
 
 __all__ = ['ruby']
 
-from ophyd import (Component, Device, EpicsMotor, EpicsSignal, PVPositioner,
-                   EpicsSignalRO, FormattedComponent)
+from ophyd import (Component, Device, EpicsMotor, EpicsSignal, EpicsSignalRO,
+                   FormattedComponent)
 from ..framework import sd
-from .util_components import DoneSignal
+from .util_components import PVPositionerSoftDone
 from ..session_logs import logger
 logger.info(__file__)
 
 
-class DAC(PVPositioner):
+class DAC(PVPositionerSoftDone):
     """ Setup DAC as a PVPositioner """
 
     setpoint = Component(EpicsSignal, '.VAL', put_complete=True, kind='normal')
     readback = Component(EpicsSignalRO, '_rbv.VAL', auto_monitor=True,
                          kind='hinted')
 
-    done = Component(DoneSignal, value=1)
-    done_value = 1
-
     low_limit = Component(EpicsSignal, '.DRVL', kind='config')
     high_limit = Component(EpicsSignal, '.DRVH', kind='config')
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self._tolerance = 0.001
-
-        self.readback.subscribe(self.done.get)
-        self.setpoint.subscribe(self.done.get)
-
-    @property
-    def tolerance(self):
-        return self._tolerance
-
-    @tolerance.setter
-    def tolerance(self, value):
-        if value < 0:
-            raise ValueError('Tolerance needs to be >= 0.')
-        else:
-            self._tolerance = value
-            _ = self.done.get()
+        super().__init__(*args, tolerance=0.001, **kwargs)
 
     @property
     def limits(self):
