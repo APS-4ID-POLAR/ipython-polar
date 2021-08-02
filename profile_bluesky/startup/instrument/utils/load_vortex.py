9b0e13af6d5d688be20e0aba0648a1f69136d06f
Gilberto Fabbris <gfabbris@anl.gov>
2021-08-02 13:38:55 -0500

    add option to use nsls2 in load_vortex

--- profile_bluesky/startup/instrument/utils/load_vortex.py
+++ profile_bluesky/startup/instrument/utils/load_vortex.py
@@ -1,6 +1,7 @@
 """ Loads a new vortex device """
 
 from ..devices.xspress import Xspress3Vortex1Ch, Xspress3Vortex4Ch
+from ..devices.xspress_nsls2 import Xspress3Vortex4Ch as nsls2
 from ..framework import sd
 from ..session_logs import logger
 logger.info(__file__)
@@ -8,7 +9,7 @@ logger.info(__file__)
 __all__ = ['load_vortex']
 
 
-def load_vortex(electronic, num_channels, num_rois=2):
+def load_vortex(electronic, num_channels, num_rois=2, which="original"):
     """
     Load the vortex detector.
 
@@ -27,11 +28,20 @@ def load_vortex(electronic, num_channels, num_rois=2):
         Vortex device.
     """
 
+    source = {
+        "original": Xspress3Vortex4Ch,
+        "nsls2": nsls2,
+    }
+
+    if which not in source.keys():
+        raise ValueError(f"which has to be in {source.keys()}, but f{which} "
+                         "was entered.")
+
     if electronic == 'xspress3':
         if num_channels == 1:
             vortex = Xspress3Vortex1Ch('XSP3_1Chan:', name='vortex')
         elif num_channels == 4:
-            vortex = Xspress3Vortex4Ch('S4QX4:', name='vortex')
+            vortex = source[which]('S4QX4:', name='vortex')
         else:
             raise ValueError('num_channels must be 1 or 4.')
         # Disable all but ROI 1 and 2
