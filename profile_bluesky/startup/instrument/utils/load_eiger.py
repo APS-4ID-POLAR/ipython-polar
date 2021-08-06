e011d0a3589a0e97b812e0f710646b523cd0321a
Gilberto Fabbris <gfabbris@anl.gov>
2021-08-06 17:30:16 -0500

    tweak load_eiger

--- profile_bluesky/startup/instrument/utils/load_eiger.py
+++ profile_bluesky/startup/instrument/utils/load_eiger.py
@@ -1,9 +1,6 @@
 """ Loads a new eiger device """
 
-from ..devices.ad_eiger import (
-        LocalEigerDetector, EIGER_FILES_ROOT, TEST_IMAGE_DIR
-)
-from os.path import join
+from ..devices.ad_eiger import LocalEigerDetector
 from ..framework import sd
 from ..session_logs import logger
 logger.info(__file__)
@@ -11,10 +8,10 @@ logger.info(__file__)
 __all__ = ['load_eiger']
 
 
-def load_eiger(pv = "dp_eiger_xrd92:"):
-    eiger = LocalEigerDetector("dp_eiger_xrd92:", name="eiger")
+def load_eiger(pv="dp_eiger_xrd92:"):
+    eiger = LocalEigerDetector(pv, name="eiger")
     sd.baseline.append(eiger)
-    
+
     eiger.wait_for_connection(timeout=10)
     # This is needed otherwise .get may fail!!!
     for name in eiger.component_names:
@@ -26,25 +23,7 @@ def load_eiger(pv = "dp_eiger_xrd92:"):
             stat = getattr(eiger, name)
             stat.wait_for_connection(timeout=10)
             stat.nd_array_port.put(f"ROI{stat.port_name.get()[-1]}")
-    
-    # start with image saving off
-    # eiger.save_images_off()
-    
-    # Default parameters
-    eiger.cam.num_triggers.put(1)
-    eiger.cam.manual_trigger.put("Disable")
-    eiger.cam.trigger_mode.put("Internal Enable")
-    eiger.cam.acquire.put(0)
-    eiger.cam.wait_for_plugins.put("Yes")
-    eiger.setup_manual_trigger()
-#    eiger.netcdf.file_path(join(EIGER_FILES_ROOT, TEST_IMAGE_DIR))
-#    eiger.netcdf.file_name.put("image")
-#    eiger.netcdf.file_template.put("%s%s_%5.5.nc")
-    eiger.cam.fw_compression.put("Enable")
-    eiger.cam.fw_num_images_per_file.put(1)
-    eiger.save_images_off()
-    # eiger.cam.file_path.put(join(EIGER_FILES_ROOT, TEST_IMAGE_DIR))
-    
-    
-    
+
+    eiger.default_kinds()
+    eiger.default_settings()
     return eiger
