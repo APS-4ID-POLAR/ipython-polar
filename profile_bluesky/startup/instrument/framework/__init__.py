210b94dd53af7ca0e29788a580448ed0b97bcf30
Gilberto Fabbris <gfabbris@anl.gov>
2022-04-05 21:34:17 -0500

    load polartools.absorption functions

--- profile_bluesky/startup/instrument/framework/__init__.py
+++ profile_bluesky/startup/instrument/framework/__init__.py
@@ -10,3 +10,14 @@ from .initialize import *
 # from .user_dir import *
 from .metadata import *
 from .callbacks import *
+
+from polartools.absorption import(
+    load_absorption,
+    load_dichro,
+    load_lockin,
+    load_multi_dichro,
+    load_multi_lockin,
+    load_multi_xas,
+    process_xmcd,
+    plot_xmcd
+)
