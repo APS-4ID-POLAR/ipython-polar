5a8f374457e80eb5238e38cf4b8e81ac685719a9
Gilberto Fabbris <gfabbris@anl.gov>
2022-05-26 13:22:37 -0500

    setup imports from polartools and hklpy

--- profile_bluesky/startup/instrument/collection.py
+++ profile_bluesky/startup/instrument/collection.py
@@ -18,6 +18,52 @@ from .utils import *
 from .mpl import *
 
 from apstools.utils import *
+from hkl.util import *
+from hkl.user import *
+
+from polartools.absorption import (
+    load_absorption,
+    load_dichro,
+    load_lockin,
+    load_multi_dichro,
+    load_multi_lockin,
+    load_multi_xas,
+    process_xmcd,
+    plot_xmcd
+)
+
+from polartools.diffraction import (
+    fit_peak,
+    load_info,
+    fit_series,
+    load_series,
+    get_type,
+    load_mesh,
+    plot_2d,
+    plot_fit,
+    load_axes,
+    plot_data,
+    dbplot,
+)
+
+from polartools.load_data import (
+    db_query,
+    show_meta,
+    collect_meta,
+    lookup_position,
+)
+
+from polartools.pressure_calibration import (
+    xrd_calibrate_pressure
+)
+
+from polartools.process_images import (
+   load_images,
+   get_curvature,
+   get_spectrum,
+   get_spectra,
+)
+
 
 from IPython import get_ipython
 from .utils.local_magics import LocalMagics
