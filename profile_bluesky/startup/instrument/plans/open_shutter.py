14e6c4048e477cbd1922f0a59bd73db7f840d0a5
Gilberto Fabbris <gfabbris@anl.gov>
2022-09-30 14:29:33 -0500

    shuters use string to open/close

--- profile_bluesky/startup/instrument/plans/open_shutter.py
+++ profile_bluesky/startup/instrument/plans/open_shutter.py
@@ -19,11 +19,11 @@ signal = SetSignal(name='tmp', timeout=30)
 def shopen(hutch="d"):
 
     if hutch.lower() == "a":
-        args = (ashutter, 1)
+        args = (ashutter, "open")
     elif hutch.lower() == "b":
-        args = (ashutter, 1, bshutter, 1)
+        args = (ashutter, "open", bshutter, "open")
     elif hutch.lower() == "d":
-        args = (ashutter, 1, bshutter, 1, dshutter, 1)
+        args = (ashutter, "open", bshutter, "open", dshutter, "open")
     else:
         raise ValueError("Input must be 'a', 'b', or 'd'.")
 
@@ -35,9 +35,11 @@ def shopen(hutch="d"):
                 0 if (shutter == ashutter) and (permit == "bleps") else 1
             )
             status = yield from rd(getattr(shutter, permit))
-            
+
             if status != good:
-                raise ValueError(f"{getattr(shutter, permit).name} is not enabled!")
+                raise ValueError(
+                    f"{getattr(shutter, permit).name} is not enabled!"
+                )
 
         # Wait for search to end. Timeout is defined above.
         function = _difference_check(1, tolerance=0.1)
@@ -50,13 +52,13 @@ def shopen(hutch="d"):
 
 def shclose(hutch="d"):
     if hutch.lower() == "a":
-        args = (ashutter, 0)
+        args = (ashutter, "close")
     elif hutch.lower() == "b":
-        args = (bshutter, 0)
+        args = (bshutter, "close")
     elif hutch.lower() == "d":
-        args = (dshutter, 0)
+        args = (dshutter, "close")
     elif hutch.lower() == "all":
-        args = (ashutter, 0, bshutter, 0, dshutter, 0)
+        args = (ashutter, "close", bshutter, "close", dshutter, "close")
     else:
         raise ValueError("Input must be 'a', 'b', 'd', or 'all'.")
 
